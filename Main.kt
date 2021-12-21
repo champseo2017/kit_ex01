30ff8e3b3656ce3fd96e33457dc90b2b0442d8a8
boomgt <32265715+champseo2017@users.noreply.github.com>
2021-12-21 22:11:07 +0700

    When

--- Main.kt
+++ Main.kt
@@ -1,8 +1,56 @@
 fun main(args: Array<String>) {
-// in !in
- val range = 1..10 step 2
- var b = 1 in range
-b = 10 in range
- b = 5 !in range
- b = 11 in 10..12
+/* When
+* val ext = ".kt"
+    when (ext) {
+      ".kt" -> print("Kotlin File")
+      ".java" -> print("Java File")
+      else -> print("Unknown")
+    }
+*
+* val country = "Mexico"
+    when (country) {
+        "UK", "USA" -> print("English")
+        "Spain", "Mexico" -> print("Spanish")
+        "China", "Taiwan" -> print("Chinese")
+    }
+    * val date = 1
+    val month = 10
+    when {
+        date == 1 && month == 1 -> print("วันขึ้นปีใหม่")
+        date == 14 && month == 2 -> print("วันวาเลนไทน์")
+        date >= 13 && date <= 15 && month == 4 -> print("วันสงกรานต์")
+        date == 1 || date == 16 -> print("วันนี้รวย")
+    }
+    *
+    * val score = 75
+    var grade = 'F'
+    when (score) {
+        in 80..100 -> grade = 'A'
+        in 79 downTo 60 -> grade = 'B'
+        in 0 until 60 -> grade = 'F'
+        !in 0..100 -> {
+            grade = ' '
+            print("Score Error")
+        }
+    }
+    *
+    *  var a: Any = 123.45F
+    var result: Int
+    when (a) {
+        is String -> result = a.toDouble().toInt()
+        is Double -> result = a.toInt()
+        is Float -> result = a.toInt()
+        else -> result = 0
+    }
+    *
+    * val device = "Oppo"
+    val os = when (device) {
+        "Oppo", "Vivo", "Samsung", "Sony" -> "Android"
+        "iPhone" -> "iOS"
+        "iPad" -> "iPadOS"
+        else -> "Unknown"
+    }
+    print(os)
+* */
+
 }
