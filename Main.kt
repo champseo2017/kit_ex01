1adb71a1510ec2a4e7c257c2a00828d4ceda1549
boomgt <32265715+champseo2017@users.noreply.github.com>
2021-12-25 13:46:10 +0700

    Pair, Triple, Array (arrayOf)

--- Main.kt
+++ Main.kt
@@ -1,32 +1,37 @@
 fun main(args: Array<String>) {
 /*
-for loop
-var str = ""
-    for (i in 1..4) {
-        str = ""
-        for (j in 1..i) {
-            str += "$j" + " "
-        }
-        println(str)
-    }
+Pair ***
+val size = Pair(50, 100)
+    val width = size.first
+    val heigth = size.second
 
-    var sum = 0
-    for (i in 1..10) {
-        if (i == 5) {
-            break
-        }
-        sum += i
-    }
-    print(sum)
+    val name = Pair("James", "Bond")
+    val msg = "Hi ${name.first} ${name.second}"
 
-    var sum = 0
-    for (i in 1..10) {
-        if (i == 5) {
-            continue
-        }
-        sum += i
-    }
-    print(sum)
+    val error = Pair(404, "Not Found")
+    print("Error: ${error.first} - ${error.second}")
+
+    val (latitude, longitude) = Pair(80.1234, 100.5678)
+    print(latitude)
+
+    val (firstname, lastname) = Pair("James", "Bond")
+    print("Hi $firstname $lastname")
+
+    Triple **
+    val boxSize = Triple(10, 20, 30)
+    val volume = boxSize.first * boxSize.second * boxSize.third
+
+    val (date, month, year) = Triple(1, "มกราคม", 2564)
+    print("วันนี้ตรงกับ $date $month $year")
+
+    val (hour, minute, _) = Triple(12, 30, 0)
+    print("เวลา $hour: $minute")
+
+    Array arrayOf *** กำหนดสมาชิกเริ่มแรก
+    val oddNumbers = arrayOf(1, 3, 5, 7, 9)
+    val vowels = arrayOf('a', 'e', 'i', 'o', 'u')
+    val data = arrayOf(1, true, "three")
+    val emptyArray = arrayOf()
 * */
 
 }
