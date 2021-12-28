61a27f61ce78608bf89b0b6cfbebade667625f60
boomgt <32265715+champseo2017@users.noreply.github.com>
2021-12-28 21:53:56 +0700

    String การตรวจสอบ string

--- Main.kt
+++ Main.kt
@@ -1,51 +1,52 @@
 fun main(args: Array<String>) {
 /*
-Pair ***
-val size = Pair(50, 100)
-    val width = size.first
-    val heigth = size.second
+    String
+    val str: String = "Kotlin"
+    val strLen = str.length
+    val strUpper = str.uppercase()
 
-    val name = Pair("James", "Bond")
-    val msg = "Hi ${name.first} ${name.second}"
+    val strLen = "Hello".length
+    val strUpper = "Android Studio".uppercase()
 
-    val error = Pair(404, "Not Found")
-    print("Error: ${error.first} - ${error.second}")
+    var str = "Android Studio"
+    var strLen = str.count()
+    str = "เยี่ยม"
+    strLen = str.length
 
-    val (latitude, longitude) = Pair(80.1234, 100.5678)
-    print(latitude)
+    var str = "Intellij IDEA"
+    var s1 = str.lowercase()
+    val s2 = str.uppercase()
 
-    val (firstname, lastname) = Pair("James", "Bond")
-    print("Hi $firstname $lastname")
+    str = "android studio"
+    val s3 = str.replaceFirstChar { it.uppercase() }
 
-    Triple **
-    val boxSize = Triple(10, 20, 30)
-    val volume = boxSize.first * boxSize.second * boxSize.third
+    str = "Android Studio"
+    val s4 = str.replaceFirstChar{ it.lowercase() }
+    print(s4)
 
-    val (date, month, year) = Triple(1, "มกราคม", 2564)
-    print("วันนี้ตรงกับ $date $month $year")
+    var str = "   Kotlin    "
+    var s1 = str.trim()
+    var s2 = str.trimStart()
+    var s3 = str.trimEnd()
 
-    val (hour, minute, _) = Triple(12, 30, 0)
-    print("เวลา $hour: $minute")
+    str = "\n \r\t Hello \r\r\t\n"
+    var s4 = str.trim()
+    print(s4)
 
-    Array arrayOf *** กำหนดสมาชิกเริ่มแรก
-    val oddNumbers = arrayOf(1, 3, 5, 7, 9)
-    val vowels = arrayOf('a', 'e', 'i', 'o', 'u')
-    val data = arrayOf(1, true, "three")
-    val emptyArray = arrayOf()
+    val str1 = ""
+    val a = str1.isEmpty()
+    val b = str1.isBlank()
 
-    การสร้างอาร์เรย์ของชนิดข้อมูลพื้นฐาน
-    val oddNums: IntArray = IntArray(3);
-    oddNums[0] = 1
-    oddNums[1] = 2
-    oddNums[2] = 3.5 // Erro ต้องเป็น Int ตามชื่อคลาสเท่านั้น
-    oddNums[3] = 4 // Error สมาชิกมีได้เพียง 3 ตัว
-    val vowels = CharArray(5)
-    vowels[0] = 'e'
+    val str2 = " "
+    val c = str2.isEmpty()
+    val d = str2.isBlank()
 
-    val evenNums: IntArray = intArrayOf(2, 4, 6, 8, 10)
-    val doubleNums = doubleArrayOf(1.23, 4.56, 7.89)
-    val vowels = charArrayOf('a', 'e', 'i', 'o', 'u')
-    val floatNums: FloatArray = floatArrayOf(11.12f, 13.14f, 555f)
+    val str3 = "\n"
+    val e = str2.isEmpty()
+    val f = str2.isBlank()
+    val g = " \n ".isBlank()
+    val h = " _ ".isBlank()
+    print(h)
 * */
 
 }
