0beb1f43e68d74e92ac0611460c9d65a3abd31c8
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-02 14:03:04 +0700

    การค้นหาตำแหน่ง, การตรวจสอบจุดเริ่มต้นและสิ้นสุดสตริง

--- Main.kt
+++ Main.kt
@@ -47,6 +47,49 @@ fun main(args: Array<String>) {
     val g = " \n ".isBlank()
     val h = " _ ".isBlank()
     print(h)
+
+    การตรวจสอบจุดเริ่มต้นและสิ้นสุดสตริง
+
+    val url = "http://www.developerthai.com"
+    if (url.startsWith("http://") || url.startWith("https://")) {
+    } else if (url.endsWith(".com")) {
+    }
+
+    val img = "logo.png"
+    if (img.endsWith("PNG", ignoreCase=true)) {
+    }
+
+    var str = "Kotlin"
+    var s = str.take(3) // ตัดเอาสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวแรก
+    s = str.takeLast(3) // ตัดเอาสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวสุดท้าย
+
+    s = str.drop(3) // ตัดสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวแรกออกไป แล้วผลลัพธ์ที่ได้คือสตริงที่เหลืออยู่
+    s = str.dropLast(3) // ตัดสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวสุดท้ายออกไป
+
+    s = str.substring(3) // ส่วนของสตริงนับตั้งแต่ลำดับที่กำหนด ไปจนสิ้นสุดสตริง
+    s = str.substring(0, 3) // ส่วนของสตริงจากลำดับเริ่มต้นจนถึง ก่อน ลำดับที่ระบุ
+    s = str.substring(1..4) // ส่วนของสตริงจากลำดับเริ่มต้นจนถึง
+    // ก่อน ลำดับที่ระบุ โดยกำหนดช่วงในแบบ range
+
+    str = "No Pain No Gain"
+    s = str.substringAfter("No") // ส่วนของสตริงที่อยู่ถัดจากสตริงที่ระบุซึ้งเจอ
+    // เป็นครั้งแรกไปจนจบสตริง
+    s = str.substringAfterLast("No") // ส่วนของสตริงที่อยู่
+    // ถัดจากสตริงที่ระบุซึ้งเจอเป็นครั้งสุดท้ายไปจนจบสตริง
+
+    str = "I Love You, I Need You"
+    s = str.substringBefore("You") // ส่วนของสตริงที่อยู่ก่อนสตริงที่ระบุ
+    // ซึ้งเจอเป็นครั้งแรกไปจนจบสตริง
+    s = str.substringBeforeLast("You") // ส่วนของสตริงที่อยู่ก่อนสตริงที่
+   // ระบุซึ้งเจอเป็นครั้งสุดท้ายไปจนจบสตริง
+
+    --- การค้นหาตำแหน่ง
+    val str = "No Pain No Gain"
+    var idx = str.indexOf("No")
+    idx = str.lastIndexOf("No")
+    idx = str.indexOf("PAIN")
 * */
+
+
 
 }
