3e8c722351b6561cc648d1c0d07d1f57932cfaab
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-02 14:11:18 +0700

    การแทนที่สตริง, การแยกและรวมสตริง

--- Main.kt
+++ Main.kt
@@ -1,93 +1,20 @@
 fun main(args: Array<String>) {
 /*
-    String
-    val str: String = "Kotlin"
-    val strLen = str.length
-    val strUpper = str.uppercase()
-
-    val strLen = "Hello".length
-    val strUpper = "Android Studio".uppercase()
-
-    var str = "Android Studio"
-    var strLen = str.count()
-    str = "เยี่ยม"
-    strLen = str.length
-
-    var str = "Intellij IDEA"
-    var s1 = str.lowercase()
-    val s2 = str.uppercase()
-
-    str = "android studio"
-    val s3 = str.replaceFirstChar { it.uppercase() }
-
-    str = "Android Studio"
-    val s4 = str.replaceFirstChar{ it.lowercase() }
-    print(s4)
-
-    var str = "   Kotlin    "
-    var s1 = str.trim()
-    var s2 = str.trimStart()
-    var s3 = str.trimEnd()
-
-    str = "\n \r\t Hello \r\r\t\n"
-    var s4 = str.trim()
-    print(s4)
-
-    val str1 = ""
-    val a = str1.isEmpty()
-    val b = str1.isBlank()
-
-    val str2 = " "
-    val c = str2.isEmpty()
-    val d = str2.isBlank()
-
-    val str3 = "\n"
-    val e = str2.isEmpty()
-    val f = str2.isBlank()
-    val g = " \n ".isBlank()
-    val h = " _ ".isBlank()
-    print(h)
-
-    การตรวจสอบจุดเริ่มต้นและสิ้นสุดสตริง
-
-    val url = "http://www.developerthai.com"
-    if (url.startsWith("http://") || url.startWith("https://")) {
-    } else if (url.endsWith(".com")) {
-    }
-
-    val img = "logo.png"
-    if (img.endsWith("PNG", ignoreCase=true)) {
-    }
-
-    var str = "Kotlin"
-    var s = str.take(3) // ตัดเอาสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวแรก
-    s = str.takeLast(3) // ตัดเอาสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวสุดท้าย
-
-    s = str.drop(3) // ตัดสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวแรกออกไป แล้วผลลัพธ์ที่ได้คือสตริงที่เหลืออยู่
-    s = str.dropLast(3) // ตัดสตริงตามจำนวนที่ระบุโดยเริ่มจากตัวสุดท้ายออกไป
-
-    s = str.substring(3) // ส่วนของสตริงนับตั้งแต่ลำดับที่กำหนด ไปจนสิ้นสุดสตริง
-    s = str.substring(0, 3) // ส่วนของสตริงจากลำดับเริ่มต้นจนถึง ก่อน ลำดับที่ระบุ
-    s = str.substring(1..4) // ส่วนของสตริงจากลำดับเริ่มต้นจนถึง
-    // ก่อน ลำดับที่ระบุ โดยกำหนดช่วงในแบบ range
-
-    str = "No Pain No Gain"
-    s = str.substringAfter("No") // ส่วนของสตริงที่อยู่ถัดจากสตริงที่ระบุซึ้งเจอ
-    // เป็นครั้งแรกไปจนจบสตริง
-    s = str.substringAfterLast("No") // ส่วนของสตริงที่อยู่
-    // ถัดจากสตริงที่ระบุซึ้งเจอเป็นครั้งสุดท้ายไปจนจบสตริง
-
-    str = "I Love You, I Need You"
-    s = str.substringBefore("You") // ส่วนของสตริงที่อยู่ก่อนสตริงที่ระบุ
-    // ซึ้งเจอเป็นครั้งแรกไปจนจบสตริง
-    s = str.substringBeforeLast("You") // ส่วนของสตริงที่อยู่ก่อนสตริงที่
-   // ระบุซึ้งเจอเป็นครั้งสุดท้ายไปจนจบสตริง
-
-    --- การค้นหาตำแหน่ง
+    การแทนที่สตริง
     val str = "No Pain No Gain"
-    var idx = str.indexOf("No")
-    idx = str.lastIndexOf("No")
-    idx = str.indexOf("PAIN")
+    val s1 = str.replaceFirst("No", "More")
+    val s2 = str.replace("No", "More")
+    val s3 = str.replace("no", "More") // เหมือนเดิม เพราะตัวพิมพ์ไม่ตรงกัน
+
+    การแยกและรวมสตริง
+    val date = "20/01/2020"
+    val d = date.split("/")
+
+    val url = "www.google.com"
+    val u = url.split(".")
+
+    val dt = arrayOf("21", "06", "2020")
+    val s = dt.joinToString("-") // s = 21-06-2020
 * */
 
 
