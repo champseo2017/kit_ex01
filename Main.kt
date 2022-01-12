23e391236ed169704f9693a986cbf15fb0156b9b
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-12 21:12:24 +0700

    ขอบเขตตัวแปรที่อยู่ในและนอก Function , การออกจาก Function

--- Main.kt
+++ Main.kt
@@ -4,23 +4,31 @@ import java.text.NumberFormat
 import java.text.DecimalFormat
 fun main(args: Array<String>) {
 /*
-   Function แบบ Single Expression
+    การออกจาก Function
+    fun showRectArea(width: Double, height: Double) {
+        if (width <= 0 || height <= 0) {
+            return
+        } else {
+            print(width * height)
+        }
+    }
 
-   fun write(str: String) = println(str);
-    fun inc(a: Int) = a + 1
-    fun add(x: Int, y: Int) = x + y
-    fun oddEven(n: Int) = if (n % 2 == 0) "Even" else "Odd"
-    fun winner(p1Score: Int, p2Scroe: Int) = when {
-        p1Score > p2Scroe -> "Player1"
-        p1Score < p2Score -> "Player2"
-        else -> "Draw"
+    fun circleArea(radius: Double) : Double =
+        if (radius < 0) 0.0 else 3.141 * radius * radius
+
+        ขอบเขตตัวแปรที่อยู่ในและนอก Function
+        val a: Int = 99
+    fun test() {
+        val b = 100
+        println(a) // ok
+        println(c) // ok
     }
-    fun numDays(month: Int, isLeapYear: Boolean) = when (month) {
-        1, 3, 5, 7, 8, 10, 12 -> 31
-        4, 6, 9, 11 -> 30
-        2 -> if (isLeapYear) 29 else 28
-        else -> 0
+    val c: Int = 101
+    fun main() {
+        test()
+        println(b) // error
     }
+
 * */
 
 }
