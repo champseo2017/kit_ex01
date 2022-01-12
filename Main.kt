255e3114debe78af0812e4a7e0a29588e8034b68
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-12 21:04:05 +0700

    Function แบบ Single Expression

--- Main.kt
+++ Main.kt
@@ -4,44 +4,23 @@ import java.text.NumberFormat
 import java.text.DecimalFormat
 fun main(args: Array<String>) {
 /*
-    fun max(num1: Int, num2: Int) : Int {
-        if (num1 >= num2) {
-            return num1
-        } else if (num1 < num2) {
-            return num2
-        } else { // ไม่มี else error
-            return num1
-        }
+   Function แบบ Single Expression
+
+   fun write(str: String) = println(str);
+    fun inc(a: Int) = a + 1
+    fun add(x: Int, y: Int) = x + y
+    fun oddEven(n: Int) = if (n % 2 == 0) "Even" else "Odd"
+    fun winner(p1Score: Int, p2Scroe: Int) = when {
+        p1Score > p2Scroe -> "Player1"
+        p1Score < p2Score -> "Player2"
+        else -> "Draw"
     }
-    fun numDaysOfMonth(month: Int, isLeapYear: Boolean): Int {
-        when (month) {
-            1, 3, 5, 7, 8, 10, 12 -> return 31
-            4, 6, 9, 11 -> return 30
-            2 -> return if (isLeapYear) 29 else 28
-            else -> return 0 // ไม่มีตรงนี้จะเกิด Error
-        }
-    }
-    fun min(num1: Int, num2: Int): Int {
-        return if (num1 <= num2) num1 else num2
-    }
-    fun result(myScore: Int, yourScore: Int): String {
-        return if (myScore > yourScore) {
-            "I Win"
-        } else if (myScore < yourScore) {
-            "You Win"
-        } else {
-            "Draw"
-        }
-    }
-    fun numDaysOfMonth(month: Int, isLeapYear: Boolean): Int {
-        return when (month) {
-            1, 3, 5, 7, 8, 10, 12 -> 31
-            4, 6, 9, 11 -> 30
-            2 -> if (isLeapYear) 29 else 28
-            else -> 0
-        }
+    fun numDays(month: Int, isLeapYear: Boolean) = when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        4, 6, 9, 11 -> 30
+        2 -> if (isLeapYear) 29 else 28
+        else -> 0
     }
 * */
 
 }
-    
