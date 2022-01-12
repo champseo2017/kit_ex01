df0249a42829c426b5b51a4af7d467c349351e17
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-12 20:56:01 +0700

    functions

--- Main.kt
+++ Main.kt
@@ -4,68 +4,44 @@ import java.text.NumberFormat
 import java.text.DecimalFormat
 fun main(args: Array<String>) {
 /*
-    Function & Lambda
-    fun printOneToTen() {
-        for (i in 1..10) {
-            println(i)
+    fun max(num1: Int, num2: Int) : Int {
+        if (num1 >= num2) {
+            return num1
+        } else if (num1 < num2) {
+            return num2
+        } else { // ไม่มี else error
+            return num1
         }
     }
-    fun oddEven(num: Int) {
-            if (num % 2 == 0) {
-                print("Even")
-            } else {
-                print("Odd")
-            }
-        }
-
-        fun repeatString(str: String, times: Int, separator: String) {
-        if (str != "" && times > 0) {
-            var text = ""
-            for (i in 1..times) {
-                if (i > 1) {
-                    text += separator
-                }
-                text += str
-            }
-            print(text)
+    fun numDaysOfMonth(month: Int, isLeapYear: Boolean): Int {
+        when (month) {
+            1, 3, 5, 7, 8, 10, 12 -> return 31
+            4, 6, 9, 11 -> return 30
+            2 -> return if (isLeapYear) 29 else 28
+            else -> return 0 // ไม่มีตรงนี้จะเกิด Error
         }
     }
-
-    fun thaiLongDate(day: Int, month: Int, yearCE: Int) {
-        // เปลี่ยนแปลงค่าของ พารามิเตอร์
-        var m = arrayOf("มกราคม")
-        // yearCE += 543 Error
-        var yearBE = yearCE
-        yearBE += 543
-        print("$day ${m[month - 1] } $yearBE")
+    fun min(num1: Int, num2: Int): Int {
+        return if (num1 <= num2) num1 else num2
     }
-
-    การเรียกใช้ฟังก์ชัน
-    fun main() {
-      printSawasdee()
-    }
-    fun printSawasdee() {
-        print("สวัสดี")
-    }
-
-    การส่งผลลัพธ์กลับจากฟังก์ชัน
-* */
-    val n : Int = add(10, 20)
-    val m = add(40, 50)
-    val friday = thaiDayName(6)
-    println("วันนี้ตรงกับวัน ${thaiDayName(1)}")
-    println("30 + 40 = ${add(30, 40)}")
-}
-    fun add(n1: Int, n2: Int) : Int {
-        val a = n1 + n2
-        return a
-    }
-
-    fun thaiDayName(dayOfWeek: Byte) : String {
-        val dayNames = arrayOf("อาทิตย์", "อาทิตย์", "อาทิตย์", "อาทิตย์", "อาทิตย์", "อาทิตย์", "อาทิตย์")
-        if (dayOfWeek in 1..7) {
-            return  dayNames[dayOfWeek - 1]
+    fun result(myScore: Int, yourScore: Int): String {
+        return if (myScore > yourScore) {
+            "I Win"
+        } else if (myScore < yourScore) {
+            "You Win"
         } else {
-            return ""
+            "Draw"
         }
     }
+    fun numDaysOfMonth(month: Int, isLeapYear: Boolean): Int {
+        return when (month) {
+            1, 3, 5, 7, 8, 10, 12 -> 31
+            4, 6, 9, 11 -> 30
+            2 -> if (isLeapYear) 29 else 28
+            else -> 0
+        }
+    }
+* */
+
+}
+    
