a83eebcd959957740fcdd182b1766d10d3fc4456
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-14 23:22:43 +0700

    การระบุชื่ออาร์กิวเมนต์ Named Argument

--- Main.kt
+++ Main.kt
@@ -4,31 +4,26 @@ import java.text.NumberFormat
 import java.text.DecimalFormat
 fun main(args: Array<String>) {
 /*
-    การออกจาก Function
-    fun showRectArea(width: Double, height: Double) {
-        if (width <= 0 || height <= 0) {
-            return
-        } else {
-            print(width * height)
-        }
-    }
-
-    fun circleArea(radius: Double) : Double =
-        if (radius < 0) 0.0 else 3.141 * radius * radius
-
-        ขอบเขตตัวแปรที่อยู่ในและนอก Function
-        val a: Int = 99
-    fun test() {
-        val b = 100
-        println(a) // ok
-        println(c) // ok
-    }
-    val c: Int = 101
-    fun main() {
-        test()
-        println(b) // error
-    }
+    การระบุชื่ออาร์กิวเมนต์
+    Named Argument
 
 * */
+    showInfo(married = true, name = "John", salary = 30_000, age= 30)
+    // OK กำหนดชื่ออาร์กิวเมนต์ครบทุกตัว จึงสามารถสลับตำแหน่งได้
+
+    // Error showInfo(Married=true, name="John", salary=30_000, age=30)
+    // Error เพราะรูปแบบตัวพิมพ์ของชื่ออาร์กิวเมนต์ ไม่ตรงกับของพารามิเตอร์
+
+    // showInfo(married=true, "John", 30_000, age=30)
+    // Error อาร์กิวเมนต์ทุกตัวที่ไม่ระบุชื่อ ต้องอยู่ก่อนตัวระบุชื่อ
+
+    showInfo("John", 30, married=true, salary = 30_000)
+    // OK อาร์กิวเมนต์ทุกตัวที่ไม่ระบุชื่อ ต้องจัดเรียงเหมือนพารามิเตอร์
+    // และอยู่ก่อนตัวที่ระบุชื่อทั้งหมด ซึ้งตัวที่ระบุชื่อสามารถสลับตำแหน่งกันได้
 
 }
+
+    fun showInfo (name: String, age: Int, married: Boolean, salary: Int) {
+        val m = if (married) "แต่งงานแล้ว" else "โสด"
+        print("ชื่อ: $name, อายุ: $age, สถานภาพ: $m, เงินเดือน: $salary")
+    }
