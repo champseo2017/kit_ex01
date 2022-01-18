0faf5acb7013629eb301d84070ea61eeeff24262
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-18 22:23:57 +0700

    Type Inference

--- Main.kt
+++ Main.kt
@@ -3,10 +3,21 @@ import kotlin.random.*;
 import java.text.NumberFormat
 import java.text.DecimalFormat
 /*
-    พารามิเตอร์ it
+    Type Inference
+    val inc: (Int) -> Int = {it + 1}
+    val add: (Int, Int) -> Int = {n1, n2 -> n1 + n2}
+    val divide: (Double, Double) -> Double = {n1, n2 -> n1 / n2}
+    val oe: (Int) -> String = {if (it % 2 == 0) "Even" else "Odd"}
+    val echo: (String) -> Unit = {println(it)}
+    val ชื่อแลมป์ดา: (ชนิดข้อมูลพารามิเตอร์) -> ชนิดข้อมูลผลลัพธ์ =
+    {พารามิเตอร์ -> คำสั่งต่างๆ}
+    val inc = {n: Int -> n + 1}
+    val add = {n1: Int, n2: Int -> n1 + n2}
+    val divide = {n1: Double, n2: Double -> n1 / n2}
+    val oe = {n: Int -> if (n % 2 == 0) "Event" else "Odd"}
+    val echo = {str: String -> println(str)}
+    val sawasdee = {print("สวัสดี")}
 * */
 fun main(args: Array<String>) {
-    val inc: (Int) -> Int = {it + 1}
-    val oe: (Int) -> String = {if (it % 2 == 0) "Event" else "Odd"}
-    val echo: (String) -> Unit = {println(it)}
+
 }
