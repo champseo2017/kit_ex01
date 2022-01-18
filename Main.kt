1b6a1d7773224a2994ee2759af7baf699ad8839c
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-18 22:00:56 +0700

    รูปแบบของแลมป์ดา

--- Main.kt
+++ Main.kt
@@ -3,22 +3,21 @@ import kotlin.random.*;
 import java.text.NumberFormat
 import java.text.DecimalFormat
 /*
-   ฟังก์ชันแบบโอเวอร์โหลด
+    รูปแบบของแลมป์ดา
 * */
 fun main(args: Array<String>) {
-    val a = max(10, 20) // ฟังก์ชัน max(Int, Int) จะถูกเรียก
-    val b = max(10.5, 20.5) // ฟังก์ชัน max(Double, Double) จะถูกเรียก
-    val c = max(108, 1009, 189) // ฟังกชัน max(Int, Int, Int)
-    val d = max(9.9, 10.9, 11.9)
+    val inc: (Int) -> Int = {n: Int -> n + 1}
+    // แลมป์ดานี้รับพารามิเตอร์ชนิด Int เข้ามา 1 ตัว จากนั้นบวกเพิ่มอีก 1 แล้ว ส่งค่ากลับเป็นชนิด Int
+    val add: (Int, Int) -> Int = {n1: Int, n2: Int -> n1 + n2}
+    val divide: (Double, Double) -> Double =
+        {n1: Double, n2: Double -> n1 / n2}
+    val oddEven: (Int) -> String =
+        {n: Int -> if (n % 2 == 0) "Even" else "Odd"}
+    val echo: (String) -> Unit = {str: String -> println(str)}
+    val sawasdee: () -> Unit = {print("สวัสดี")}
+    val a = inc(10)
+    val b = add(20, 30)
+    val c = oddEven(99)
+    echo("Hello")
+    sawasdee()
 }
-    fun max(n1: Int, n2: Int) = if (n1 >= n2) n1 else n2
-    fun max(n1: Int, n2: Int, n3: Int): Int {
-        var mx = n1
-        mx = if (n2 > mx) n2 else mx
-        mx = if (n3 > mx) n3 else mx
-        return mx
-    }
-    fun max(n1: Double, n2: Double) = if (n1 >= n2) n1 else n2
-    fun max(n1: Double, n2: Double, n3: Double) : Double {
-        // เช่น เดียวชนิด Int
-    }
