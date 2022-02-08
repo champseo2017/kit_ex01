48f6b333081dca4150edf5fd8cdcdb1faf6e997a
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-02-08 10:02:41 +0700

    การใช้ Safe Call Operator

--- Main.kt
+++ Main.kt
@@ -1,52 +1,35 @@
-import kotlin.math.*
-import kotlin.random.*;
-import java.text.NumberFormat
-import java.text.DecimalFormat
-/*
-   การใช้งานตัวแปรที่เป็น Nullable
-     การตรวจสอบด้วย if
-         val x: Int? = 10
-    if (x != null) {
-        println("x = $x")
-    } else {
-        println("x = null")
-    }
-    print("Enter number >>")
-    val str: String? = readLine()
-    if (str != null) {
-        val n: Double? = str.toDoubleOrNull()
-        if (n != null) {
-            print("n = $n")
-        } else {
-            print("Error! can't convert to a number")
-        }
-    } else {
-        print("Error!")
-    }
+import java.util.*
 
-    การใช้ Elvis Operator
-     - หากตัวแปรมีค่าเป็น null ก็ให้ใช้ข้อมูลอย่างอื่นแทนค่าของตัวแปร (ค่าดีฟอลต์)
-     - NullableVar ?: Default
-        - NullableVar คือตัวแปรหรือสิ่งที่มีโอกาสเป็น null ได้
-        - Default คือค่า ดีฟอลต์กรณีที่ NullableVar มีค่าเป็น null
-         val a: Int? = 0
-        val x = a ?: 0
-        /*
-        *  เทียบเท่ากับ
-        *  val x = if (a != null) a else 0
-        * */
-        val b: Double? = 1.23
-        val y = b ?: -1.0
-        print("Enter number >>")
-        val input: String? = readLine()
-        val str = input ?: ""
-        val n1: Int? = 10
-        val n2: Int? = null
-        val n: Int
-        n = (n1 ?: 0) + (n2 ?: -1)
-        val m = ("123".toIntOrNull() ?: 0) * 100
+/*
+*   การใช้ Safe Call Operator
+*    - val str: String? = readLine()
+*    - val n = str.toDouble() // Error เพราะ str อาจเป็น null ได้
+*    - val d: Double? = 10.5
+*     - val i: Int = d.toInt() // Error เพราะ d อาจเป็น null ได้
+*
+*      .? หากตัวแปรไม่มีค่าเป็น null จะเรียกใช้ เมธอด ถ้าเป็น null จะไม่ถูกเรียก และ
+*      ตัวแปรนั้นจะมีค่าเป็น null โดยอัตโนมัติ ตัวแปรที่ใช้เก็บผลลัพธ์ จึงต้องมี Type เป็น
+*      null เช่นกัน
+*
 * */
 fun main(args: Array<String>) {
-    val msg: String? = readLine()
-    print(msg ?: "Error")
+    val str: String? = readLine()
+    val n: Double? = str?.toDouble() // OK
+    /*  เทียบเท่ากับ
+    *  if (str != null) {
+    *   val n = str.toDouble()
+    * }
+    *
+    * */
+    val d: Double? = 10.5
+    val i: Int? = d?.toInt()
+
+    val s = str?.trim()?.substring(0, 10)?.uppercase(Locale.getDefault())
+    // จากโค๊ด ตัวแปร str ต้องไม่เป็น null เมธอดทั้งหมดจึงจะถูกเรียกไปตามลำดับ
+    // สามารถประยุกต์ใช้ self call Operator ร่วมกับ Elvis Operator เช่น
+    val str2: String? = readLine()
+    val len = (str2?.length) ?: 0
+    // จากโค็ด len ถ้าไม่มีค่าเป็น null ตัวแปร len จะมีค่า length แต่ถ้ามีค่าเป็น null
+    // ตัวแปร len จะมีค่าเป็น 0 จากตัวดำเนินการ ?: (Elvis Operator)
+
 }
