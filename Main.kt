739fac8721f60a6d551cf1a840d04a09b5a7d586
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-02-15 09:36:34 +0700

    การใช้วิธี Not Null Assertion Operator

--- Main.kt
+++ Main.kt
@@ -1,31 +1,18 @@
 import java.util.*
+// การใช้วิธี Not Null Assertion Operator
+// ถ้าเรามั่นใจว่าตัวแปรดังกล่าว จะไม่เป็น null อย่างแน่นอน ก็สามารถจัดการด้วยโอเปอเรเตอร์
+// Not Null Assertion (!!) แทนวิธีการอื่นๆ วิธีนี้ต้องแน่ใจจริงๆ ว่าตัวแปรนั้นไม่เป็นค่า null
 
-/*
-    การใช้ฟังก์ชัน ?.let {}
-    กำหนดสิ่งที่จะดำเนินการ หากตัวแปรไม่มีค่าเป็น null การใช้ฟังก์ชัน let โดยเรียกผ่าน
-    ตัวแปรที่ทำการตรวจสอบด้วยโอเปอเรเตอร์ Safe Call (?.) แล้วกำหนดการกระทำไว้ใน
-    บล็อก {} เช่น
-    print("Please enter string >>")
-    val str: String? = readLine()
-    str?.let {
-        print("string => $str")
-    }
-    // เทียบเท่ากับการตรวจสอบด้วย if เช่น
-    /*
-    if (str != null) {
-        print("string => $str")
-    }
-    * */
-* */
 fun main(args: Array<String>) {
-    print("Please enter website >>")
-    val str: String? = readLine()
-    var str1: String = ""
-    val url = str?.let {
-        // ไม่ใช้ null ?.let จึงดำเนินการ
-        if (!it.startsWith("http://") || (!it.startsWith("https://"))) {
-            str1 = "https//$str"
-        }
-    }
-    println(str1 ?: "")
+//    // วางโอเปอเรเตอร์ !! ไว้หลังตัวแปรแบบ Nullable และสามารถใช้งานเหมือนกับตัวแปร
+//    // ทั่วไปได้เลยโดยไม่ต้องตรวจสอบอีก เช่น
+//    val a: Int? = 10
+//    val b: Int? = 20
+//    val c: Int = a!! + b!!
+//    // อาจใช้โอเปอเรเตอร์  !!  ร่วมกับ . ในการเรียกเมธอดของตัวแปรนั้นแทนการใช้โอเปอเรเตอร์
+//    // Safe Call เช่น
+//    val str: String? = "123"
+//    val x: Int = str!!.toInt()
+//    print(x)
+
 }
