5e59ce1a5fd7559393f16e8cf087fef8d9f3ea9a
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-03 14:03:43 +0700

    Init Block

--- Main.kt
+++ Main.kt
@@ -2,31 +2,20 @@ import java.util.*
 import java.*;
 import kotlin.random.*;
 
-// เมธอด constructor อาจเป็น Overload ก็ได้
-// นั่นคือ เราสามารถสร้างเมธอดด้วยชื่อเดี่ยวกันนี้ได้มากกว่า 1 อัน
-// เพื่อรับข้อมูลในลักษณะที่ต่างๆ กัน เราต้องสร้างพร็อปเพอร์ตี้เพื่อจัดเก็บข้อมูลให้ครอบ
-// คลุมพารามิเตอร์ทั้งหมดของเมธอด constructor ทุกอัน เช่น
-
-class Rectangle {
-    var width: Int
-    var height: Int
-    constructor(width: Int, height: Int) {
-        this.width = if (width > 0) width else 0
-        this.height = if (height > 0) height else 0
-    }
-    constructor(width: Int) {
-        this.width = if (width > 0) width else 0
-        this.height = if (width > 0) width else 0
-        // แม้คอนสตรักเตอร์อันนี้ จะไม่รับค่า height เข้ามา
-        //แต่เราต้องกำหนดค่าให้ครอบคลุมพร็อปเพอร์ตี้ทุกตัว
-    }
-    fun getArea() : Int = this.width * this.height
-}
+// Init Block
+// กรณีที่เราต้องการกระทำบางอย่างเมื่ออินสแตนซ์ของคลาสถูกสร้างขึ้น ก็สามารถกำหนด
+// คำสั่งไว้ใน บล็อก init เช่น กรณีที่เลือกคอนสตรักเตอร์แบบ Primary อาจใช้บล็อก init
+// ในการตรวจสอบความถูกต้องของข้อมูลที่รับเข้ามา หรือใช้กำหนดการกระทำอื่นๆ
+// ตามที่เราต้องการ
+/*
+*  class Circle(var radius: Int, val PI: Double=3.14159) {
+*   init {
+*    radius = if (radius > 0 ) radius else 0
+* }
+* }
+* */
 
 
 fun main(args: Array<String>) {
-    val rect1 = Rectangle(width = 10, height = 15)
-    println(rect1.getArea())
-    val rect2 = Rectangle(width = 10)
-    println(rect2.getArea())
+
 }
