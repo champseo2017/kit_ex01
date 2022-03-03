e327f864bc10a6839c6fb3171351f8a63e63e3ef
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-03 14:25:11 +0700

    ใช้ private กับ Primary Constructor แบบพร็อปเพอร์ตี้

--- Main.kt
+++ Main.kt
@@ -1,35 +1,16 @@
 import java.util.*
 import java.*;
 import kotlin.random.*;
-// โมดิฟายเออร์แบบ private
-// โมดิฟายเออร์แบบ private จะใช้เมื่อเราต้องกำจัดสิทธิ์ให้สามารถเข้าถึงได้เฉพาะภายใน
-// คลาสของมันเองเท่านั้น แต่ไม่อณุญาติให้เข้าถึงจากภายนอกคลาส
-// หากเป็นพร็อปเพอร์ตี้ที่ใช้สำหรับการจัดเก็บข้อมูลอย่างเดียว (ไม่มีบล็อก get/set)
-// เราควรป้องกันการเข้าถึงจากภายนอกคลาสด้วย private เพื่อป้องกันปัญหาการกำหนด
-// ข้อมูลที่ผิดหลักการ
-/*
-*  class Triangle {
-*   private var base: Int
-*   private var height: Int
-*   constructor(base: Int, height: Int) {
-*    this.base = if (base > 0) base else 0
-*    this.height = if (height > 0) height else 0
-*  }
-*   fun getArea() : Double = 0.5 * base * height
-* }
-*
-*
-* */
-class Triangle {
-    private  var base: Int
-    private  var height: Int
-    constructor(base: Int, height: Int) {
-        this.base = if (base > 0) base else 0
-        this.height = if (height > 0) height else 0
+// ใช้ private กับ Primary Constructor แบบพร็อปเพอร์ตี้ ก็มีผลในลักษณะเดียวกับ
+// พร็อปเพอร์ตี้แบบปกติ
+class Circle(private var radius: Int) {
+    init {
+        radius = if (radius > 0) radius else 0
     }
-    fun getArea(): Double = 0.5 * base * height
+    fun getArea() = 3.14 * radius * radius
+    fun getPerimeter() = 2 * 3.14 * radius
 }
 fun main(args: Array<String>) {
-  val t = Triangle(10, 15)
-   //  t.base = 12  Error เพราะ base เป็นพร็อปเพอร์ตี้แบบ private
+    val c = Circle(10)
+    // c.radius = 20 // Error เพราะ radius เป็นพร็อปเพอร์ตี้แบบ private
 }
