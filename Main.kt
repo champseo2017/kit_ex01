e789650b4a25e3061976b17352589afecf886125
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-03 14:19:15 +0700

    โมดิฟายเออร์แบบ private 1

--- Main.kt
+++ Main.kt
@@ -1,21 +1,35 @@
 import java.util.*
 import java.*;
 import kotlin.random.*;
-
-// Init Block
-// กรณีที่เราต้องการกระทำบางอย่างเมื่ออินสแตนซ์ของคลาสถูกสร้างขึ้น ก็สามารถกำหนด
-// คำสั่งไว้ใน บล็อก init เช่น กรณีที่เลือกคอนสตรักเตอร์แบบ Primary อาจใช้บล็อก init
-// ในการตรวจสอบความถูกต้องของข้อมูลที่รับเข้ามา หรือใช้กำหนดการกระทำอื่นๆ
-// ตามที่เราต้องการ
+// โมดิฟายเออร์แบบ private
+// โมดิฟายเออร์แบบ private จะใช้เมื่อเราต้องกำจัดสิทธิ์ให้สามารถเข้าถึงได้เฉพาะภายใน
+// คลาสของมันเองเท่านั้น แต่ไม่อณุญาติให้เข้าถึงจากภายนอกคลาส
+// หากเป็นพร็อปเพอร์ตี้ที่ใช้สำหรับการจัดเก็บข้อมูลอย่างเดียว (ไม่มีบล็อก get/set)
+// เราควรป้องกันการเข้าถึงจากภายนอกคลาสด้วย private เพื่อป้องกันปัญหาการกำหนด
+// ข้อมูลที่ผิดหลักการ
 /*
-*  class Circle(var radius: Int, val PI: Double=3.14159) {
-*   init {
-*    radius = if (radius > 0 ) radius else 0
+*  class Triangle {
+*   private var base: Int
+*   private var height: Int
+*   constructor(base: Int, height: Int) {
+*    this.base = if (base > 0) base else 0
+*    this.height = if (height > 0) height else 0
+*  }
+*   fun getArea() : Double = 0.5 * base * height
 * }
-* }
+*
+*
 * */
-
-
+class Triangle {
+    private  var base: Int
+    private  var height: Int
+    constructor(base: Int, height: Int) {
+        this.base = if (base > 0) base else 0
+        this.height = if (height > 0) height else 0
+    }
+    fun getArea(): Double = 0.5 * base * height
+}
 fun main(args: Array<String>) {
-
+  val t = Triangle(10, 15)
+   //  t.base = 12  Error เพราะ base เป็นพร็อปเพอร์ตี้แบบ private
 }
