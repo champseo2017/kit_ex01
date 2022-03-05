e9babb2b31b59f487364a18178efb172a0833fee
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-05 14:08:09 +0700

    หากกำหนด private ให้แก่เมธอด จะไม่สามารถเข้าถึงได้จากภายนอกคลาส

--- Main.kt
+++ Main.kt
@@ -1,16 +1,28 @@
 import java.util.*
 import java.*;
 import kotlin.random.*;
-// ใช้ private กับ Primary Constructor แบบพร็อปเพอร์ตี้ ก็มีผลในลักษณะเดียวกับ
-// พร็อปเพอร์ตี้แบบปกติ
-class Circle(private var radius: Int) {
-    init {
-        radius = if (radius > 0) radius else 0
+// หากกำหนด private ให้แก่เมธอด จะไม่สามารถเข้าถึงได้จากภายนอกคลาส
+class ThaiDate(private var day: Int, private var month: Int, private var year: Int) {
+     init {
+         if (!validDate(day, month, year)) {
+             day = 0; month = 0; year = 0
+         }
+     }
+    private val m = arrayOf("มกราคม")
+    fun getMonthName() : String ? {
+        return if (month in 1..12) m[month - 1] else null
     }
-    fun getArea() = 3.14 * radius * radius
-    fun getPerimeter() = 2 * 3.14 * radius
+    private fun validDate(d: Int, m: Int, y: Int) : Boolean {
+        return try {
+            java.time.LocalDate.of(y, m, d)
+            true // ถ้าไม่ผิดพลาด ก็คืนค่า true
+        } catch (ex: Exception) {
+            false
+        }
+    }
 }
 fun main(args: Array<String>) {
-    val c = Circle(10)
-    // c.radius = 20 // Error เพราะ radius เป็นพร็อปเพอร์ตี้แบบ private
+    val thDate = ThaiDate(31, 13, 2020)
+    print(thDate.getMonthName() ?: "Error")
+    // if (thDate.validDate(32, 1, 31)) {} // Error (เพราะเป็น private)
 }
