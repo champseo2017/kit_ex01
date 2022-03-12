a92512703a54bc8eb7a7d0b953a706ce1737659f
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-12 21:26:26 +0700

    การสืบทอดจากคลาส 1

--- Main.kt
+++ Main.kt
@@ -1,32 +1,50 @@
 import java.util.*
 import java.*;
 import kotlin.random.*;
-// การใช้ฟังก์ชัน apply กับสมาชิกของคลาส
-class Spy  {
-    var name = ""
-    var code = ""
-    var yearBorn = 0
+
+// เช่น ต้องการสืบทอดจากคลาสที่ชื่อ OldClass ก็เขียนโ้ดได้ดังนี้
+// class NewClass: OldClass () {}
+// Kotlin ไม่สนับสนุนการสืบทอดจากคลาสคลาส (Multiple Inheritance) แต่ละคลาส
+// จะสืบทอดได้เพียงคลาสเดียวเท่านั้น
+// คลาสต่างๆ จะสืบทอดต่อเนื่องกันกี่ชั้นก็ได้ หากระบุโมดิฟายเอร์ open เช่น คลาส B สืบ
+// ทอดจาก A และคลาส C สืบทอดจาก B เป็นต้น
+/*
+* open class A { // โมดิฟายเอร์ open เพื่อบ่งชี้ว่า คลาสนี้สามารถสืบทอดได้
+* }
+* open class B : A() {
+* }
+* class C : B() {
+* }
+*
+* class D : C() {} // Error เพราะคลาส C ไม่ระบุโมดิฟายเออร์ open จึงสืบทอดไม่ได้
+*
+* สมมุติมี 2 คลาส คลาส Person และ Programmer
+*
+*
+*
+* */
+open class Person {
+    var name: String = ""
+    var yearBorn: Int = 0
     fun getAge() = java.time.LocalDate.now().year - yearBorn
 }
-/*
-* val spy = Spy()
-    spy.name = "James Bond"
-    spy.code = "007"
-    spy.yearBorn = 1980
-    println(spy.name)
-    println(spy.code)
-    println(spy.getAge())
-* จากโค้ด จะเห็นได้ว่า เราต้องระบุอินสแตนซ์เดิมซ้ำๆ หลายครั้ง ดังนั้นใน Kotlin จึงมีฟังก์ชัน
-*  apply
-* */
+//class Programmer {
+//    var name: String = ""
+//    var yearBorn: Int = 0
+//    var salary: Int = 0
+//    var expertLanguage: String = ""
+//    fun getAge() = java.time.LocalDate.now().year - yearBorn
+//}
+// จากโค้ด เราให้คลาส Person แทนคน คลาส Programmer แทนลักษณะของคนที่เป็น
+// โปรแกรมเมอร์ จะเห็นว่า Person จะซ้ำกับ Programmer แต่หากเราเปลี่ยนมาใช้วิธีการสืบ
+// ทอด โดยให้คลาส Programmer สืบทอดจากคลาส Person
+class Programmer: Person() {
+    var salary: Int = 0
+    var expertLanguage: String = ""
+}
 fun main(args: Array<String>) {
-    val spy = Spy()
-    spy.apply {
-        name = "James Bond"
-        code = "007"
-        yearBorn = 1980
-        println(name)
-        println(code)
-        println(getAge())
-    }
+    var pro = Programmer()
+    pro.yearBorn = 1995
+    println(pro.getAge())
+    pro.salary = 50_000
 }
