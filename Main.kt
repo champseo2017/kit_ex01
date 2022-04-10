a4ea3ba10a12b08869691f56d963760ae450d3a1
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-04-10 21:55:05 +0700

    กรณีที่ Superclass มีเฉพาะ Primary Constructor

--- Main.kt
+++ Main.kt
@@ -2,33 +2,30 @@ import java.util.*
 import java.*;
 import kotlin.random.*;
 
-/* คอนสตรักเตอร์กับการสืบทอด
-ตามหลักการของ Kotlin เมื่อสืบทอดจากคลาสใด ต้องส่งข้อมูลให้แก่คอนสตรักเตอร์ของคลาสนั้นเสมอ
+/* กรณีที่ Superclass มีเฉพาะ Primary Constructor
+หากเราจะสืบทอดจากคลาสดังกล่าว ที่ Subclass ต้องกำหนดคอนสตรักเตอร์แบบ Primary หรือ Secondary ที่มีรูปแบบ
+การรับข้อมูลสอดคล้องกับคอนสตรักเตอร์ของ Superclass แล้วส่งข้อมูลดังกล่าวให้กับ Superclass
 
-* กรณีที่ Superclass ไม่มีคอนสตรักเตอร์
-* ทุกๆ คลาสต้องมี Default Constructor เสมอ ซึ้งลักษณะของมันคือ ไม่มีพารามิเตอร์
-* ใดๆ คอมไฟลเลอร์จะสร้างให้เองโดยอัตโนมัติ
-* 1. ใส่เป็นวงเล็บ () ว่างๆ ต่อท้ายชื่อ Superclass ซึ้งหมายถึงการเรียก Default Constructor
-    ของ Superclass
-
-  2. สร้างคอนสตรักเตอร์แบบ Secondary แล้วระบุคำสั่ง super() ต่อท้าย
-*
 * */
 
-open class Person {
-    // คลาสนี้มีเฉพาะ Default Constructor
+// พร็อปเพอร์ตี้ที่ Subclass ต้องกำหนดชื่อพร็อปเพอร์ตี้ให้แตกต่าง
+open class Device(var os: String) {
+
 }
 
-class Programmer : Person() { // เรียก Default Constructor ของ Person
+class Tablet(var osName: String) : Device(osName) {
+    // ส่งพร็อปเพอร์ตี้ osName ให้แก่คอนสตรักเตอร์ของ Device
+    // ต้องกำหนดชื่อพร็อปเพอร์ตี้ที่รับข้อมูลให้ต่างจากของ Superclass
 }
 
-class Customer : Person { // ไม่ต้องมีวงเล็บ ()
-    constructor(name: String) : super() {} // เรียก Default Constructor
+class Smartphone(osName: String, brandName: String) : Device(osName) {
+    // ส่งแค่พารามิเตอร์ osName ที่สอดคล้องกันให้แก่คอนสตรักเตอร์ของ Device
+    // หรือจะกำหนดเป็นพร็อปเพอร์ตี้ก็ได้
 }
 
-class Guest : Person {
-    // ถ้าในคอนสตรักเตอร์แบบ Secondary ไม่มีคำสั่งใดๆ ไม่ต้องเขียน {} ก็ได้
-    constructor() : super()
+class Phablet: Device { // กรณีสร้างเป็น secondary constructor
+    constructor(osName: String) : super(osName) {
+    }
 }
 
 fun main(args: Array<String>) {
