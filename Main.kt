1329fe21b150e04428dcb3b0f443673f1cddba6d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-04-11 21:05:15 +0700

    Superclass มีเฉพาะ Secondary Constructor

--- Main.kt
+++ Main.kt
@@ -2,29 +2,29 @@ import java.util.*
 import java.*;
 import kotlin.random.*;
 
-/* กรณีที่ Superclass มีเฉพาะ Primary Constructor
-หากเราจะสืบทอดจากคลาสดังกล่าว ที่ Subclass ต้องกำหนดคอนสตรักเตอร์แบบ Primary หรือ Secondary ที่มีรูปแบบ
-การรับข้อมูลสอดคล้องกับคอนสตรักเตอร์ของ Superclass แล้วส่งข้อมูลดังกล่าวให้กับ Superclass
+/* Superclass มีเฉพาะ Secondary Constructor
+
+สืบทอดคลาสที่มีเฉพาะคอนสตรักเตอร์แบบ Secondary ใช้วิธีเดียวกับกรณีที่คลาสนั้นมี
+เพียงคอนสตรักเตอร์แบบ Primary
 
 * */
 
-// พร็อปเพอร์ตี้ที่ Subclass ต้องกำหนดชื่อพร็อปเพอร์ตี้ให้แตกต่าง
-open class Device(var os: String) {
+open class Device {
+    constructor(os: String) {
 
+    }
 }
 
 class Tablet(var osName: String) : Device(osName) {
-    // ส่งพร็อปเพอร์ตี้ osName ให้แก่คอนสตรักเตอร์ของ Device
-    // ต้องกำหนดชื่อพร็อปเพอร์ตี้ที่รับข้อมูลให้ต่างจากของ Superclass
 }
 
-class Smartphone(osName: String, brandName: String) : Device(osName) {
-    // ส่งแค่พารามิเตอร์ osName ที่สอดคล้องกันให้แก่คอนสตรักเตอร์ของ Device
-    // หรือจะกำหนดเป็นพร็อปเพอร์ตี้ก็ได้
+class Smartphone(osName: String, brandName: String)
+    : Device(osName) {
 }
 
-class Phablet: Device { // กรณีสร้างเป็น secondary constructor
-    constructor(osName: String) : super(osName) {
+class Phablet: Device {
+    constructor(osName: String): super(osName) {
+
     }
 }
 
