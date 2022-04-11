f87ad5462da5475b949fc0cdd1818606e096857a
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-04-11 22:06:54 +0700

    การสร้างคอนสตรักเตอร์เพิ่มเติมให้แก่ Subclass

--- Main.kt
+++ Main.kt
@@ -3,44 +3,28 @@ import java.*;
 import kotlin.random.*;
 
 /*
-    Superclass มีทั้ง Primary และ Secondary Constructor
-    ถ้าคลาสมีคอนสตรักเตอร์มากกว่า 1 อัน เมื่อสร้างอินสแตนซ์ของมันต้องกำหนดพารามิเตอร์ให้ตรงกับ
-    รูปแบบของคอนสตรักเตอร์อย่างใดอย่างหนึ่งของคลาส
-
-    การสืบทอด โดยที่ Subclass ต้องมีคอนสตรักเตอร์แบบ Primary หรือ Secondary อย่างน้อย 1 อัน
+   การสร้างคอนสตรักเตอร์เพิ่มเติมให้แก่ Subclass
 
 * */
 
-open class Device(var os: String) {
-    constructor(os: String, brandName: String): this(os) {
+open class Device(var os: String)
 
+class Tablet(var osName: String): Device(osName) {
+    constructor(osName: String, brandName: String): this(osName) {}
+    constructor(osName: String, osVersion: Double, brandName: String)
+            : this(osName) {
     }
 }
 
-class Tablet(var osName: String) : Device(osName) {
-    // ส่งข้อมูลคอนสตรักเตอร์แบบ Primary ของคลาส Device
-}
-
-class Smartphone(var osName: String, var brandName: String)
-    : Device(osName, brandName) {
-        // ส่งข้อมูลให้แก่คอนสตรักเตอร์แบบ Secondary ของคลาส Device
-
-}
-
-class Phablet: Device {
-    constructor(osName: String, brandName: String): super(osName) {
-        // ส่งข้อมูลให้แก่คอนสตรักเตอร์แบบ Primary ของคลาส Device
+open class Computer
+class NoteBook(var brand: String) : Computer() {
+    private var model = ""
+    constructor(brand: String, model: String,
+                processor: String, memory: Int, display: Double) : this(brand) {
+                this.model = model
     }
 }
 
-class Watch: Device {
-    constructor(osName: String, brandName: String): super(osName, brandName) {
-
-    }
-    // ส่งข้อมูลให้แก่คอนสตรักเตอร์แบบ Secondary ของคลาส Device
-}
-
-// สามารถเลือกใช้ระหว่าง Primary และ Secondary
 
 fun main(args: Array<String>) {
 
