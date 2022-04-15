36aca008d242496e57724d1b43d7101e673a75ec
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-04-16 00:39:44 +0700

    Object Expression

--- Main.kt
+++ Main.kt
@@ -3,74 +3,58 @@ import java.*;
 import kotlin.random.*;
 
 /*
-    การสร้างและใช้งานอินเทอร์เฟซ
+    Object Expression
 
-    - อินเทอร์เฟซ (Interface) ประกอบด้วยเมธอด หรือ พร็อปเพอร์ตี้ โดยไม่ได้ระบุค่าและวิธีการดำเนินการ
-    เอาไว้ เพื่อให้คลาสที่สืบทอดไปเป็นผู้กำหนดค่าและวิธีดำเนินการเอาเอง Interface คล้ายกับ Abstract
-    ทั่วไปเราใช้อินเทอร์เฟซมากกว่าคลาสแบบ Abstract
+    ถ้าต้องการใช้งานอินเทอร์เฟซไปใช้งาน ต้องสร้างคลาสขึ้นมาสืบทอด ซึ้งจะเกิดความยุ่งยาก
+    เมื่อใช้ อินเทอร์เฟซจำนวนมาก
 
-    การสร้างอินเทอร์เฟซ
-    หลักการสร้างอินเทอร์เฟซใน Kotlin มีดังนี้
-     - รูปแบบพื้นฐานของอินเทอร์เฟซคือ
-       interface ชื่ออินเทอร์เฟซ { // ชื่ออินเทอร์เฟซใช้หลักการเดียวกับชื่อคลาส
-            // สมาชิกของอินเทอร์เฟซ
-       }
+    Anonymous (Inner) Class
 
-
-     - ในแต่ละคลาส สามารถสือทอดอินเทอร์เฟสได้พร้อมกันมากกว่า 1 อัน
-     คลาสกับคลาสสืบทอดจากคลาสอื่นได้เพียงอันเดียว
-
-     - ในแต่ละคลาส สามารถสืบทอดพร้อมกันทั้งคลาสและอินเทอร์เฟซได้
-
-     interface OS
-     interface Camera
-     interface Display
-     open class Device
-
-     class Mobile: OS, Camera, Display
-
-     class Smartphone: Device(), OS, Camera, Display
+    object: interface_name {
+        // โอเวอร์ไรด์สมาชิกของอินเทอร์เฟซที่เป็น Abstract ตามปกติ
+    }
 
 * */
 
-interface Shape3D {
-    val PI: Double // Abstract Property
-    fun surfaceArea(): Int // Abstract Method
-    fun volume(): Int // Abstract Method
-    fun shapeName(): String = "" // not Abstract method
+interface Math {
+    fun add(a: Int, b: Int): Int
+    fun subtract(a: Int, b: Int): Int
 }
 
-// การสืบทอดและใช้งานอินเทอร์เฟซ
-class Box (var w: Int, var l: Int, var h: Int): Shape3D {
-    override var PI = 3.14
-    override fun volume() : Int = w * l * h
-    override fun surfaceArea() = (w * l * 2) + (w * h * 2) + (l * h * 2)
+// function หรือ class รับพารามิเตอร์เป็นอินเทอร์เฟซ
+fun calculate(x: Int, y: Int, math: Math) { // Math เป็นอินเทอร์เฟซ
+    println("$x + $y = ${math.add(x, y)}")
+    println("$x - $y = ${math.subtract(x, y)}")
 }
 
-// อินเทอร์เฟซ สืบทอดจากอินเทอร์เฟซด้วยกันได้
-interface  Login {
-    var email: String
-    var password: String
+interface MouseClick {
+    fun onClick()
 }
 
-interface User: Login {
-    var firstName: String
-    var lastName: String
-    fun name() = "$firstName $lastName"
+class Button {
+    fun onMouseClick(m: MouseClick) {
+        m.onClick()
+    }
 }
 
-class Customer: User {
-    override var email = "..."
-    override var password = "..."
-    override var firstName = "..."
-    override var lastName = ""
-}
 
 fun main(args: Array<String>) {
 
-    val b = Box(10, 20, 30)
-    println(b.shapeName())
-    println(b.volume())
+//    var mt = object: Math {
+//        override fun add(a: Int, b: Int): Int = a + b
+//        override fun subtract(a: Int, b: Int) = a - b
+//    }
 
+    calculate(20, 5, object: Math { // object เป็นอาร์กิวเมนต์โดยตรง
+        override fun add(a: Int, b: Int): Int = a + b
+        override fun subtract(a: Int, b: Int) = a - b
+    })
+
+    val bt = Button()
+    bt.onMouseClick(object: MouseClick {
+        override fun onClick() {
+            println("Mouse Click")
+        }
+    })
 
 }
