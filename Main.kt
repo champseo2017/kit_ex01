e8a8e0a6e0885ab67c041087e285623bd0c5097e
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-04-24 13:37:47 +0700

    Generic Class
    หลักการสืบทอดของคลาสที่เป็น Generic

--- Main.kt
+++ Main.kt
@@ -3,39 +3,70 @@ import java.*;
 import kotlin.random.*;
 
 /*
-    Smart Cast
-    ตรวจสอบชนิดข้อมูลด้วย is แปลงข้อมูลด้วย as
+    Generic Class
+    เมื่อนำ Generic Class ไปใช้งาน ในขั้นตอนการสร้างอินสแตนซ์ต้องระบุชนิดข้อมูลที่แน่นอน
+    ให้กับคลาส ถ้าไม่ระบุชนิดข้อมูล คอมไพเลอร์จะพิจารณาจากค่าที่กำหนดให้แก่คอนสตรักเตอร์
 
-    fun increment(x: Any) : Int? {
-        var n: Int
-        return if (x is Int) {
-            n = x as Int
-            ++n
-        } else null
+    หาก Type ที่จะกำหนดให้แก่ Generic Class ไม่ใช้ชนิดข้อมูลพื้นฐาน เราต้องกำหนดค่าเป็นอินสแตนซ์
+    ของคลาสดังกล่าวให้กับคอนสตรักเตอร์
+
+    class Data<T>(...) {...}
+    class Test
+
+    fun main() {
+       val test = Test()
+       val data1 = Data<Test>(test)
+       val data2 = Data<Test>(Test())
+       val data3 = Data(Test())
     }
 
-    หากตรวจสอบด้วย is แล้วให้ผลลัพธ์เป็น true (เป็น type ตามที่ระบุ) ตัวแปรจะถูกแปลงเป็นชนิด
-    ที่เราตรวจสอบโดยอัตโนมัติ และ สามารถนำปใช้งานได้เลย โดยไม่จำเป็นต้องแปลงด้วย as
+    // อินสแตนซ์เดิมซึ้งได้กำหนดชนิดข้อมูล Generic Type เป็นอย่างใดอย่างหนึ่งไปแล้วไม่สามารถนำ
+    มาสร้างเพื่อกำหนด Type ใหม่ได้ เหมือนกับเราเปลียนแปลงชนิดข้อมูลของตัวแปรไม่ได้
+
+    var data = Data<Int>(value=123)
+    data = Data<String>(value="Kotlin") // Error
+    data = Data(value=true) // Error
+
+    ในบางคลาสอาจมี Generic Type มากกว่า 1 ชนิด ซึ้งเราต้องแทนด้วยตัวอักษรที่แตกต่างกัน
+    class Info<T, U, V>(val x: T, val y: U, val z: V) {
+    }
+
+    val error = Pair<Int, String>(404, "Not Found")
+    // หรือเขียนเป็น Pair(404, "Not Found")
+
+    val rgb = Triple<Int, Int, Int>(100, 150, 250)
+    // Triple(100, 150, 250)
+
+    - หลักการสืบทอดของคลาสที่เป็น Generic
+      - ถ้า Superclass เป็น Generic แล้ว Subclass ก็ต้องเป็น Generic ด้วย
+      - ถ้า Superclass ไม่เป็น Generic แต่ Subclass อาจเป็น Generic ก็ได้
+
+      open class First<T> // คลาส First เป็น Generic
+      class Second<T>: First<T>() // คลาส Second ต้องเป็น Generic ด้วย
+
+      open class Third // คลาส Third ไม่เป็น Generic
+      class Fourth<T>: Third() // คลาส Fourth อาจเป็น Generic ก็ได้
+
+      open class Fifth<T: Number> // ดู T: Number ในเรื่อง Type Constraint
+      class Sixth<T: Number>: Fifth<T>()
 
 * */
 
-   fun increment(x: Any) : Int ? {
-       var n: Int
-       return if (x is Int) {
-          n = x // x ถูกแปลงเป็นชนิด Int โดยอัตโนมัติ เมื่อเราตรวจสอบด้วย if
-          ++n
-       } else null
+    class Data<T>(value: T) {
+        private var v = value as Any
 
-       /*  หรือใช้ when
-       *   return when (x) {
-       *    is Int -> { n = x; ++n }
-       *    else -> null
-       *   }
-       * */
-   }
+         fun type() = v
 
+        fun value(): T = v as T
 
+        fun setValue(value: T) {
+            v = value as Any
+        }
+    }
 
 fun main(args: Array<String>) {
-
+//    val data1: Data<Int> = Data<Int>(100)
+//    val data2: Data<Double> = Data(12.34)
+//    val data3 = Data<String>("Hello")
+//    val data4 = Data(true) // มีชนิดข้อมูลเป็น Boolean
 }
