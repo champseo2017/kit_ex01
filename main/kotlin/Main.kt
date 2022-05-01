903f84891251291f179897579c5b69faf89281e9
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-05-01 12:36:22 +0700

    update

---
+++ main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+
+
+/*
+Generic Function อีกอันหนึ่งคือ arrayOfNulls<T>() ใช้ในการสร้่างอาร์เรย์
+ที่สมาชิกแต่ละตัว มีค่าเริ่มแรกเป็นfwefwe
+* */
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
