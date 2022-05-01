5da9dcb027329b3888f680b2dbce856e00252457
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-05-01 15:06:10 +0700

    คอลเล็กชันประเภท List
    การใช้ Lterator ร่วมกับ List

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -4,22 +4,47 @@
     แต่มีความยืดหยุ่นต่อการเปลี่ยนแปลงจำนวนสมาชิกโดยการเพิ่มหรือลบรายการได้
     ในขณะที่อาร์เรย์นั้น จำนวนสมาชิกจะคงที่ตลอด
 
-    Immutable List
-    เป็นลิสต์แบบ Read-Only คืออ่านได้อย่างเดียว แต่ไม่สามารถเปลี่ยนแปลงจำนวนสมาชิก
-    โดยการเพิ่มหรือลบได้ กำหนด Type ในแบบ Generic เป็น List<T> และ กำหนดเป็น
-    รายการสมาชิกด้วยฟังก์ชัน listOf()
+
+    การใช้ Lterator ร่วมกับ List
+    หากเราต้องการเข้าถึงสมาชิกของลิสต์แบบต่อเนื่องกันซึ้งนอกจากการใช้ลูปแบบ for-in
+    ยังสามารถใช้ Lterator ได้อีกหนึ่งวิธี โดยทั้ง Mutable และ Lmmutable list
+    ต่างก็มีเมธอดในการสร้าง lterator
+
+    iterator() คืนค่าเป็นอินสแตนซ์ของ Iterator เพื่อนำไปใช้ในการเข้าถึงรายการในลิสต์แบบ
+    เลื่อนไปข้างหน้าได้อย่างเดียว
+
+    listltorator() คืนค่ามาเป็นอินสแตนซ์ของ Iterator เพื่อนำไปใช้ในการเข้าถึงรายการในลิสต์
+    แบบเลื่อนได้ทั้งไปข้างหน้าและย้อนกลับ
+
+    หลักการใช้ Iterator ก็คือ ให้เลื่อนตำแหน่งการอ่านไปเรื่อยๆ ทีละตำแหน่ง จนกว่าจะครบตามจำนวน
+    สมาชิก (เลื่อนต่อไปไม่ได้)
+
+    - ลักษณะการใช้ Iterator ที่ได้จากเมธอด iterator()
+
 
 
 * */
 
 fun main(args: Array<String>) {
 
-    val oddNums: List<Int> = listOf<Int>(1, 3, 5, 7, 9)
-    val colors: List<String> = listOf("red", "green", "blue")
-    val mobiles = listOf<String>("sumsung", "oppo", "vivo", "sony")
-    val vowels = listOf('a', 'e', 'i', 'o', 'u')
+    val list = mutableListOf<String>("Java", "Kotlin", "Android")
+    val iter = list.iterator()
+//    while (iter.hasNext()) { // ถ้าเลื่อนต่อไปได้
+//        println(iter.next()) // อ่านค่าสมาชิกในลำดับถัดไป
+//    }
 
-    // list ประเภทนี้จะมีเฉพาะเมธอดในการอ่านหรือตรวจสอบข้อมูลเท่านั้น โดยลักษณะ เช่น
-    // เดียวกับ Mutable List
+    // - ลักษณะการใช้ Iterator ที่ได้จากเมธอด ListIterator()
+    val lists = mutableListOf<String>("Java", "Kotlin", "Android")
+    val iters = list.listIterator()
+
+    while (iters.hasNext()) {
+        println(iters.next())
+    }
+
+    println()
+
+    while (iters.hasPrevious()) { // หากเลื่อนย้อนกลับได้
+        println(iters.previous()) // อ่านค่าสมาชิกในลำดับก่อนนี้
+    }
 
 }
