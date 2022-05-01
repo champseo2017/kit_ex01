a16e0ee13d2791cba9ce826c9b60976e65d55c72
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-05-01 12:39:18 +0700

    Generic Function อีกอันหนึ่งคือ arrayOfNulls<T>()

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,7 +2,7 @@
 
 /*
 Generic Function อีกอันหนึ่งคือ arrayOfNulls<T>() ใช้ในการสร้่างอาร์เรย์
-ที่สมาชิกแต่ละตัว มีค่าเริ่มแรกเป็นfwefwe
+ที่สมาชิกแต่ละตัว มีค่าเริ่มแรกเป็น null
 * */
 
 fun main(args: Array<String>) {
