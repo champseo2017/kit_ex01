2c46182ba7ba4a2799f5eec4ad6d0f8b9ac490a0
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-05-01 23:00:48 +0700

    การใช้แลมบ์ดากับคอลเล็กชัน

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,56 +1,27 @@
 /*
-    คอลเล็กชันประเภท Map
-    Collection Type ชุดข้อมูล Dictionary สมาชิกแต่ละตัวมีส่วนประกอบ 2 อย่างคือ
-     - Key ใช้ในการอ้างถึงสมาชิกแต่ละตัว เปรียบได้กับคำศัพท์ใน Dictionary
-     - Value เป็นค่าของสมาชิกตัวนั้น เปรียบได้กับความหมายของคำศัพท์ใน Dictionary
+    การใช้แลมบ์ดากับคอลเล็กชัน
 
-     โดย Map ก็แบ่งออกเป็น 2 แบบคือ Mutable Map และ Immutable Map
-
-     การกำหนดสมชิก จะใช้คำว่า to เพื่อจับคู่ระหว่าง key และ value
-     คอลเล็กชัน Map มีเมธอดที่สำคัญสำหรับจัดการสมาชิก
-
-     put(key, value) เพิ่มสมาชิกลงใน Map
-     get(key) อ่านสมาชิกที่มีคีย์ตรงกับที่ระบุ
-     set(key, value) เพิ่มสมาชิกที่มีคีย์และค่าของมันตามที่กำหนด
-     clear() ลบสมาชิกทั้งหมดออกจาก Map
-     containsKey(key) ตรวจสอบว่ามีคีย์ที่ระบุหรือไม่ หากมีจะคืนค่า true
-     containsValue(value) ตรวจสอบว่ามีค่าที่ระบุหรือไม่ หากมีจะคืนค่า true
-     isEmpty() ตรวจว่า Map นี้ว่างเปล่าหรือไม่ ถ้าใช้จะคืนค่า true
-     remove(key) ลบสมาชิกที่มีคีย์ตรงกับที่ระบุ
-     replace(key, value) แทนที่ค่าของสมาชิกเดิมที่มีคีย์ตามที่ระบุด้วยค่าใหม่
-     size() อ่านขนาดหรือจำนวนสมาชิกใน Map
-     keys() อ่านค่าคีย์ทั้งหมด
-     values() อ่านค่าทั้งหมด (ไม่รวมคีย์)
-
-     เมธอดที่เกี่ยวกับการเพิ่มหรือลบสมาชิก เช่น put(), set(), remove() จะใช้ได้เฉพาะกับ
-     Mutable Map
-
-
-
-
+    - forEach(action: (T) -> Unit) สำหรับ List และ Set
+    - forEach(action: Map.Entry(k, v) -> Unit) สำหรับ Map
 * */
 
 fun main(args: Array<String>) {
+    val list = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val set = mutableSetOf(1.2, 3.4, 5.6, 7.8, 9.0)
+    val map = mutableMapOf<Int, String>(66 to "Thailand", 81 to "Japan"
+    , 61 to "Australia")
 
-    val c = mutableMapOf("th" to "Thailand")
-    c["jp"] = "Japan"
-    c.put("au", "Australia")
-    c.set("uk", "United Kingdom")
-//    println(c.get("th"))
-    if (c.containsKey("au")) {
-        c.remove("au")
+    list.forEach {
+        if (it % 2 == 0) println(it)
     }
 
-//    for (k in c.keys) {
-//        println(c[k]) // หรือ c.get(k)
-//    }
+    set.forEach { println(it * 10) }
 
-    val iter = c.iterator()
-    while (iter.hasNext()) {
-        // println(iter.next())
-        // iter.next().value
-        println(iter.next().value)
-        // iter.next().key
+    map.forEach {
+        if (it.key < 80) {
+            println(it.key.toString() + " : " + it.value)
+        }
     }
+
 
 }
