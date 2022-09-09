cc63e1757d38e5a2e31d36010a4521d0775b5831
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-09-09 09:26:50 +0700

    การใช้คำสั่ง if แบบ conditional expression

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,24 +1,34 @@
 /*
-    การตรวจสอบเงื่อนไขด้วย if-else
+    การใช้คำสั่ง if แบบ conditional expression
+    -สามารถใช้คำสั่ง if-else ในแบบ expression เพื่อกำหนดค่าของ
+    ตัวแปรตามเงื่อนไข
 
+    val remain = 1
+    val r = if (remain > 0) "มีสินค้า" else "สินค้าหมด"
+    print(r)
 
+    นำ if-else ไปแทรกไว้ ณ จุดที่ต้องการใช้ผลลัพธ์
+    val ext = ".kt"
+    print(if (ext == ".java") "Java File" else "Kotlin File")
 
+    เขียนคำสั่ง if ในบล็อกที่ซับซ้อน
+    val manuGoals = 2
+    val liverGoals = 3
+    val result =
+        if (manuGoals > liverGoals) {
+            "ManUnited ชนะ!"
+        } else if (manuGoals < liverGoals) {
+            "Liverpool ชนะ!"
+        } else {
+            "เสมอกัน"
+        }
+
+    print(result)
 
 * */
 
 fun main(args: Array<String>) {
 
-    val code = "abcde"
-    val n1 = 108
-    val n2 = 1009
-    val n3 = 189
-    var max = n1
-    if (n2 > max) {
-      max = n2
-    }
-    if (n3 > max) {
-       max = n3
-    }
 
-    print("จำนวนที่มากที่สุดคือ $max")
+
 }
