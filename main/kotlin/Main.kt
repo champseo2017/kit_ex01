c19fc0cb00721c60e9fe703f690230ea8d19e1af
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-09-16 09:18:19 +0700

    การเก็บข้อมูลในแบบ Pair

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -7,12 +7,6 @@
     เมื่อเราต้องการอ่านค่าสมาชิกไปใช้งาน ให้อ้างอิงผ่านชื่อ first หรือ second
     สำหรับตัวแรกและตัวที่สองตามลำดับ
 
-
-
-* */
-
-fun main(args: Array<String>) {
-
     val size = Pair(50, 100)
     val width = size.first
     val height = size.second
@@ -22,5 +16,24 @@ fun main(args: Array<String>) {
 
     val error = Pair(404, "Not Found")
     print("Error: ${error.first} - ${error.second}")
+
+    นอกจากนี้ อาจสร้างตัวแปรด้วยชื่อที่ต้องการไว้ในวงเล็บ () เพื่อ
+    ใช้แทนสมาชิกแต่ละตัวของ Pair
+
+    val (latitude, longitude) = Pair(80.1234, 100.5678)
+
+    print(latitude)
+
+    val (firstname, lastname) = Pair("James", "Bond")
+
+    print("Hi $firstname $lastname")
+
+
+
+* */
+
+fun main(args: Array<String>) {
+
+
 
 }
