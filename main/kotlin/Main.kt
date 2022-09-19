a1c5d8e5971a1bf3579dc27a2e90d0b83b1939e2
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-09-19 09:26:50 +0700

    การตัดช่องว่างออกจากสตริง

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,23 +1,21 @@
 /*
-   การเปลี่ยนรูปแบบตัวพิมพ์
+   การตัดช่องว่างออกจากสตริง
 
-   - toLowerCase() แปลงสตริงที่กำหนดให้เป็นตัวพิพม์เล็กทั้งหมด
-   - toUpperCase() แปลงสตริงที่กำหนดให้เป็นตัวพิมพ์ใหญ่ทั้งหมด
-   - capitalize() แปลงอักขระแรกของสตริง ให้เป็นตัวพิมพ์ใหญ่
-   - decapitalize() ยกเลิกการแปลงอักขระตัวแรกของสตริง เป็นตัวพิมพ์ใหญ่
+   trim() ตัดช่องว่างและอักขระประเภท escape (เช่น \n, \t) ที่อยู่ข้างหน้าและ
+   ข้างหลังสตริงแต่ไม่ตัดช่องว่างระหว่างคำภายในสตริงออก
 
-   var str = "Intellij IDEA"
-    val s1 = str.lowercase()
-    val s2 = str.uppercase()
+   trimStart() ตัดช่องว่าง escape(เช่น \n, \t) เฉพาะที่อยู่ข้างหน้าสตริง
 
+   trimEnd() ตัดช่องว่างอักขระประเภท escape(เช่น \n, \t) เฉพาะที่อยู่ข้างหลังสตริง
 
-    str = "android studio"
-    val s3 = str.replaceFirstChar({ it.uppercase() })
+    var str = "  Kotlin "
+    var s1 = str.trim()
+    var s2 = str.trimStart()
+    var s3 = str.trimEnd()
 
-    str = "Android Studio"
-    val s4 = str.replaceFirstChar({ it.lowercase() })
+    str = "\n \r\t Hello \r\r\t\n "
+    var s4 = str.trim()
 
-    println(s3)
     println(s4)
 
 * */
