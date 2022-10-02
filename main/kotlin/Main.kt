7c770fe72fee3724b4dfd93cbaa4a609a33a15e7
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-02 12:08:27 +0700

    การเขียนและการเรียกใช้ฟังก์ชัน

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,47 +1,23 @@
 /*
-    การกำหนดพารามิเตอร์ขั้นพื้นฐาน
+    การเขียนและการเรียกใช้ฟังก์ชัน
 
-    fun ชื่อฟังก์ชัน(พารามิเตอร์: ชนิดข้อมูล, ...) {
-      คำสั่งต่างๆ
-    }
+    - การเรียกฟังกชันที่มีพารามิเตอร์
+      ข้อมูลที่เรากำหนดเพื่อส่งไปยังฟังก์ชันจะเรียกว่า อาร์กิวเมนต์ (Argument)
 
-    fun oddEven(num: Int) {
-        if (num % 2 == 0) {
-            print("Even")
-        } else {
-            print("Odd")
-        }
-    }
-
-    fun repeatString(str: String, times: Int, separator: String) {
-        if (str !== "" && times > 0) {
-            var text = ""
-            for (i in 1..times) {
-                if (i > 1) {
-                    text += separator
-                }
-                text += str
-            }
-            print(text)
-        }
-    }
-
-    ใน Kotlin ไม่สามารถเปลี่ยนค่าของพารามิเตอร์
-    ภายในฟังก์ชันโดยตรงได้ หากต้องการเปลียนพารามิเตอร์ใน function
-    ให้สร้างตัวแปรด้วย var ขึ้นมาใหม่ภายในฟังก์ชัน จากนั้นกำหนดค่าของ
-    พารามิเตอร์ให้กับมัน แล้วทำการเปลียนค่าของมันแล้วนำไปใช้แทนพารามิเตอร์
+      fun test(a: Int, b: String, c: Boolean) {
+        // a, b, c คือ พารามิเตอร์ (formal parameter)
+      }
 
 * */
 
+fun test(a: Int, b: String, c: Boolean) {
+
+}
+
+
 fun main(args: Array<String>) {
 
-    fun thaiLongDate(day: Int, month: Int, yearCE: Int) {
-        var m = arrayOf("มกราคม")
-        // yearCE += 543 Error
-        var yearBE = yearCE
-        yearBE += 543
-        print("$day ${m[month-1]} $yearBE")
-    }
+    test(10, "Hello", true) // Argument
 
 
 }
