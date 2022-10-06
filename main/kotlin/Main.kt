7649ca0941938f91113f3371bfe351f5b7951e69
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-06 21:59:03 +0700

    การออกจากฟังก์ชัน

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,35 +1,22 @@
 /*
 
-   การสร้างฟังก์ชันแบบ Single Expression
-   - เราสามารถตัดวงเล็บ {} ทิ้งไป แล้วใช้เครื่องหมาย
-   = เพื่อกำหนดค่าที่จะส่งกลับต่อท้ายฟังก์ชันในบรรทัดเดียวกันได้เลย
-   เขียนเฉพาะข้อมูลที่จะส่งกลับ
+    การออกจากฟังก์ชัน
+    - วางคำสั่ง return ไว้ ณ จุดที่ต้องการออกจากฟังกชัน
 
 * */
 
-fun write(str: String) = println(str)
-// ไม่ส่งค่ากลับเพราะไม่มีผลลัพธ์เกิดขึ้น
-
-fun inc(a: Int) = a + 1
-// ส่งผลลัพธ์ของ a + 1 กลับออกไป
-
-fun add(x: Int, y: Int) = x + y
-// ส่งผลลัพธ์ของ x + y กลับออกไป
-
-fun oddEven(n: Int) = if (n % 2 == 0) "Even" else "odd"
-
-fun winner(p1Score: Int, p2Score: Int) = when {
-    p1Score > p2Score -> "Player1"
-    p1Score < p2Score -> "Player2"
-    else -> ""
+fun showReactArea(width: Double, height: Double) {
+    if (width <= 0 || height <= 0) {
+        return // fun นี้ไม่ส่งค่ากลับ จึงไม่ต้องระบุข้อมูล
+    } else {
+        print(width * height)
+    }
 }
 
-fun numDays(month: Int, isLeapYear: Boolean) = when (month) {
-    1, 3 -> 31
-    4, 5 -> 30
-    2 -> if (isLeapYear) 29 else 28
-    else -> 0
-}
+// ฟังก์ชันต่อไปนี้ต้องส่งค่ากลับ ให้ระบุค่าที่บ่งบอกว่าล้มเหลว (ในที่นี้คือ 0.0)
+
+fun circleArea(radius: Double) : Double = if (radius < 0) 0.0
+else 3.141 * radius * radius
 
 fun main(args: Array<String>) {
 
