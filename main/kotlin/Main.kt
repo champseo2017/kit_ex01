7b03ee579e3bf342cadf61f6ae2090b915e08217
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-08 15:11:36 +0700

    การส่งหลายๆ ค่ากลับจากฟังก์ชัน
    - การส่งค่ากลับแบบอาร์เรย์

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -3,13 +3,27 @@
     การส่งหลายๆ ค่ากลับจากฟังก์ชัน
 
     - การส่งค่ากลับแบบอาร์เรย์
-    
 
+      - ถ้าเป็น Array ชนิดข้อมูลพื้นฐานก็ส่งค่า Array ชนิดข้อมูลพื้นฐานออกไป
+      - ถ้าไม่ใช้ชนิดข้อมูลพื้นฐาน อาจกำหนดเป็น Array<ชนิดข้อมูล> ภายในฟังก์ชัน
+      ให้สร้างผลลัพธ์ให้เป็นอาร์เรย์ของข้อมูลชนิดนั้น หลังจากนั้นก็คืนผลลัพธ์ออกไป
 
 * */
 
+fun oneToTenEvenNums() : IntArray {
+    return intArrayOf(2, 4, 6, 8, 10)
+}
+
+fun getVowels(): Array<String> {
+    return arrayOf("a", "e", "i", "o", "u")
+}
 
 fun main(args: Array<String>) {
 
+    var vowels = getVowels()
+
+    for (v in vowels) {
+        println(v)
+    }
 
 }
