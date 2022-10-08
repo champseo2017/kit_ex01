61a63e3c86e34754f953097739738c9ae9df050a
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-08 15:55:21 +0700

    ฟังก์ชันแบบโอเวอร์โหลด

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,30 +1,41 @@
 /*
 
-    การส่งค่ากลับแบบ Pair หรือ Triple
+    ฟังก์ชันแบบโอเวอร์โหลด
+     - ชื่อฟังก์ชันต้องเขียนเหมือนกัน (รูปแบบตัวพิมพ์ต้องตรงกันทุกอักขระ)
+     - รูปแบบของพารามิเตอร์ต้องแตกต่างกัน
+       - ชนิดข้อมูลของพารามิเตอร์เหมือนกัน จำนวนพารามิเตอร์ต้องไม่เท่ากัน
+       - ชนิดข้อมูลของพารามิเตอร์ต่างกัน จำนวนพารามิเตอร์อาจเท่ากันหรือไม่ ก็ได้
 
-    - การส่งผลลัพธ์กลับในแบบ Pair หรือ Triple เป็นอีกทางเลือกหนึ่ง
-
-    - Pair<ชนิดข้อมูล_1, ชนิดข้อมูล_2>
-    - Triple<ชนิดข้อมูล_1, ชนิดข้อมูล_2, ชนิดข้อมูล_3>
+     - ชนิดข้อมูลส่งกลับอาจเหมือนหรือแตกต่างกันก็ได้
 
 * */
 
-fun summation(vararg data: Int) : Pair<Int, Double> {
 
-    var sum = 0
+fun max(n1: Int, n2: Int) = if (n1 >= n2) n1 else n2
 
-    for (n in data) {
-        sum += n
-    }
+fun max(n1: Int, n2: Int, n3: Int) : Int {
+    var mx = n1
+    mx = if (n2 > mx) n2 else mx
+    mx = if (n3 > mx) n3 else mx
+    return mx
+}
 
-    var average = sum.toDouble() / data.count().toDouble()
+fun max(n1: Double, n2: Double) = if (n1 >= n2) n1 else n2
 
-    return Pair(sum, average)
+fun max(n1: Double, n2: Double, n3: Double) : Double {
+    var mx = n1
+    mx = if (n2 > mx) n2 else mx
+    mx = if (n3 > mx) n3 else mx
+    return mx
 }
 
 fun main(args: Array<String>) {
 
-    val sum = summation(10, 15, 18, 22)
-    print("sum: ${sum.first}, average: ${sum.second}")
+ val a = max(10, 20)
+ val b = max(10.5, 20.5)
+ val c = max(108, 1009, 189)
+ val d = max(9.9, 10.9, 11.9)
+
+ print(d)
 
 }
