613092f20f1fdcae30b5ce226d18c66a4bc9b56d
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-13 12:00:28 +0700

    Type Inference

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,19 +1,28 @@
 /*
 
-    พารามิเตอร์ it
-    กรณีที่แลมบ์ดา มีพารามิเตอร์เพียงตัวเดียว ภายในสาวนของแลมบ์ดา เราไม่จำเป็น
-    ต้องระบุพารามิเตอร์ รวมทั้งสัญลักษ์ ->
-    สามารถกำหนดการกระทำต่างๆ ลงไปได้เลย
-    แทนค่าพารามิเตอร์ที่รับเข้ามาด้วยคำว่า it
+    Type Inference
+    - ใช้ Type Inference เป็นการระบุชนิดข้อมูลเพียงฝั่งใด
+    ฝั่งหนึ่ง
+
+    // แบบไม่ใช้ Type Inference
+    val inc: (Int) -> Int = { it + 1 }
+    val add: (Int, Int) -> Int = { n1, n2 -> n1 + n2 }
+    val divide: (Double, Double) -> Double = { n1, n2 -> n1 / n2 }
+    val oe: (Int) -> String = { if (it % 2 == 0) "Even" else "Odd" }
+    val echo: (String) -> Unit = { println(it) }
 
 * */
 
 
 fun main(args: Array<String>) {
 
-  val inc: (Int) -> Int = { it + 1 }
-  val oe: (Int) -> String = { if (it % 2 == 0) "Even" else "Odd" }
-  val echo: (String) -> Unit = { println(it) }
+  val inc = {n: Int -> n + 1}
+  val add = {n1: Int, n2: Int -> n1 + n2}
+  val divide = { n1: Double, n2: Double -> n1 / n2 }
+  val oe = {n: Int -> if (n % 2 == 0) "Even" else "Odd"}
+  val echo = {str: String -> println(str)}
+  val sawasdee = { print("Hello") }
+  sawasdee()
 
 
 }
