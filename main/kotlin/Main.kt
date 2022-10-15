8d4b9a9b6a06e18f8f2715156829b56047ca803a
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-15 15:14:26 +0700

    การใช้งานตัวแปรที่เป็น Nullable 1

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,61 +2,25 @@ import java.awt.Shape
 
 /*
 
-    Null Safety & Exception
+    การใช้งานตัวแปรที่เป็น Nullable
+     - หากตัวแปรมีค่าเป็น null แต่เรานำไปใช้โดยไม่มีการตรวจสอบ
+       อาจเกิดข้อผิดพลาดขึ้น เช่น
+       var a: Int? = null
+       a += 1 // Error
 
-    ลักษณะของค่า Null
-     - ไม่มีค่าใดๆ
-     - ไม่เท่ากับ 0
-     - ไม่เท่ากับ Empty String ("") และ Blank String (" ")
-     - ไม่เท่ากับ false
-     - ไม่ตรงกับชนิดข้อมูลใดๆ
-
-     ใน Kotlin ไม่สามารถนำค่า null ไปกำหนดให้กับตัวแปร หรือ ค่าคงที่ใดๆ โดยตรงได้
-     เพราะ Null ไม่ตรงกับชนิดข้อมูลใดๆ เลย
-
-     val a: Int = null // Error
-     var b: String = "Hello"
-     b = null // Error
-
-     ใน Kotlin จะใช้วิธีการแบบ Null Safety สำหรับการกำหนดชนิดข้อมูลให้กับ
-     ตัวแปรที่มีโอกาสเป็น null ได้ โดยวางเครื่องหมาย ? ต่อท้ายชนิดข้อมูลของ
-     ตัวแปรหรือค่าคงที่นั้น เช่น
-
-     var a: Int?
-     var b: Double?
-     val c: String?
-
-     // ตัวแปร a, b, c เป็นแบบ Nullable คือ นอกจากจะเก็บข้อมูลตามชนิดของมันได้
-     ปกติแล้ว ยังสามารถเป็นค่า null ได้
-
-     var a: Int? = 123
-     var b: Double? = 4.56
-     val c: String? = null
-     var d: Any? = null
-
-     d = 100
-     d = "Kotlin"
-     d = true
-
-     // ยังมีบางฟังก์ชันที่อาจคืนค่า null กลับมา เช่น readLine()
-        - toXxxOrNull()
-        - toIntOrNull()
-        - toDoubleOrNull()
+      - ใช้คำสั่ง if ตรวจสอบว่าตัวแปรมีค่าเป็น null หรือไม่
+      - Elvis Operator (?:) กำนหดค่าดีฟอลต์สำหรับกรณีที่ตัวแปรนั้นมีค่าเป็น null
+      - Not Null Assertion Operator (!!) ใช้กรณีที่เรามั่นใจว่าตัวแปรนั้น ไม่มีค่าเป็น null
+      อย่างแน่นอน
+      - call Safety (?) ใช้กับเมธอดหรือ พร็อปเพอร์ตี้ของออบเจ็ตก์ที่อาจคืนค่า null กลับมา
+      - ?.let {} ใช้ Call Safety ร่วมกับ let ดำเนินการบางอย่าง
+      หากตัวแปรนั้นไม่มีค่าเป็น null
 
 
 * */
 
 
 fun main(args: Array<String>) {
-
-   var str: String? = readln() // readLine() คืนค่าเป็น String?
-   var i: Int? = "123.45".toIntOrNull()
-   var d: Double? = "67.89MB".toDoubleOrNull()
-   var s: Short? = "".toShortOrNull()
-
-   // ถ้าไม่ระบุชนิดข้อมูลให้กับตัวแปรที่สามารถระบุค่า null ได้ จะไม่สามารถ กำหนดค่า
-   // ให้กับตัวแปรนั้นได้นอกจาก Null
-    // var a = null | a = 10 // Error
 
 
 }
