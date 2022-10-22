b864965cbd02ec36cdb3ec808ac61c3423c1537c
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-22 15:51:42 +0700

    การใช้ฟังก์ชัน ?.let {}
    - กำหนดสิ่งที่จะดำเนินการ หากตัวแปรไม่มีค่าเป็น null

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,19 +2,13 @@ import java.awt.Shape
 
 /*
 
-    การใช้งานตัวแปรที่เป็น Nullable
-     - หากตัวแปรมีค่าเป็น null แต่เรานำไปใช้โดยไม่มีการตรวจสอบ
-       อาจเกิดข้อผิดพลาดขึ้น เช่น
-       var a: Int? = null
-       a += 1 // Error
+    การใช้ฟังก์ชัน ?.let {}
+    - กำหนดสิ่งที่จะดำเนินการ หากตัวแปรไม่มีค่าเป็น null
+     ใช้ ฟังก์ชัน let โดยเรียกผ่านตัวแปรที่ทำการตรวจสอบด้วยโอเปอเรเตอร์
+     Safe Call (?.) แล้วกำหนดการกระทำไว้ในบล็อก {}
 
-      - ใช้คำสั่ง if ตรวจสอบว่าตัวแปรมีค่าเป็น null หรือไม่
-      - Elvis Operator (?:) กำนหดค่าดีฟอลต์สำหรับกรณีที่ตัวแปรนั้นมีค่าเป็น null
-      - Not Null Assertion Operator (!!) ใช้กรณีที่เรามั่นใจว่าตัวแปรนั้น ไม่มีค่าเป็น null
-      อย่างแน่นอน
-      - call Safety (?) ใช้กับเมธอดหรือ พร็อปเพอร์ตี้ของออบเจ็ตก์ที่อาจคืนค่า null กลับมา
-      - ?.let {} ใช้ Call Safety ร่วมกับ let ดำเนินการบางอย่าง
-      หากตัวแปรนั้นไม่มีค่าเป็น null
+     - let มีพารามิเตอร์เป็น functions Type ฟังก์ชั่นนี้จึงเป็นแบบแลมบ์ดา
+       สามารถนำพารามิเตอร์ it มาใช้แทนตัวแปรได้ รวมถึงไม่ต้องใช้คำสั่ง return
 
 
 * */
@@ -22,39 +16,28 @@ import java.awt.Shape
 
 fun main(args: Array<String>) {
 
-    // การใช้ Elvis Operator
-    // หากตัวแปรมีค่าเป็น null ให้ใช้ข้อมูลอย่างอื่นแทนค่าของตัวแปร
-    // เรียกว่าค่าดีฟอลต์ NullableVar ?: Default
-    // NullableVar ตัวแปรที่มีโอกาสเป็น null ได้
+//    print("Please enter string >>")
+//    val str: String? = readLine()
+//
+//    str?.let {
+//        print("string => $str")
+//    }
 
-    val a: Int? = null
-    val x = a ?: 0 // หาก a เป็น null ตัวแปร x จะเท่ากับ 0
-    // หากไม่เป็น null ตัวแปร x = a
-    println(x)
     /*
-    * เทียบเท่ากับ
-    * val x = if (a != null) a else 0
+    * เทียบเท่ากับการตรวจสอบ if เช่น
+    * if (str !== null) {
+    *  print("string => $str")
+    * }
     *
     * */
 
-    val b: Double? = 1.23
-    val y = b ?: -1.0
-    println(y)
+    print("Please enter website >>")
+    val str: String? = readLine()
+    val url = str?.let {
+        "https//$str"
+    }
 
-    print("Enter number >>")
-    val input: String? = readLine()
-    val str = input ?: ""
+    println(url ?: "")
 
-    val n1: Int? = 10
-    val n2: Int? = null
-    val n: Int
-    n = (n1 ?: 0) + (n2 ?: -1)
-    // n = 10 + (-1) = 9 เพราะ n2 มีค่าเป็น null
-
-    val m = ("123".toIntOrNull() ?: 0) * 100
-    println(m)
-
-    val msg: String? = readLine()
-    println(msg ?: "Error")
 
 }
