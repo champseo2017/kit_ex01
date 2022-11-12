b0cd3c8c8c5a91b2f048081b99d1eefe4f18c436
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-12 21:59:54 +0700

    โมดิฟายเออร์แบบ private

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,19 +2,91 @@ import kotlin.random.*
 
 /*
 
-    Init Block
-     - ทำบางอย่างเมื่ออินสแตนซ์ของคลาสถูกสร้างขึ้น สามารถกำหนดคำสั่ง
-     ไว้ใน บล็อก init
+    โมดิฟายเออร์แบบ private
+     - ใช้จำกัดสิทธิ์ให้สามารถเข้าถึงได้เฉพาะภายในคลาสของมันเองเท่านั้น
+     ไม่อนุญาตให้เข้าถึงจากภายนอกคลาสได้ ปกติจะกำหนดให้กับสมาชิก ภายในคลาส
+     เช่น เมธอด หรือ พร็อปเพอร์ตี้
 
-     class Circle(var radius: Int, val PI: Double=3.14159) {
-       init {
+     - หากเป็นพร็อปเพอร์ตี้ที่ใช้สำหรับการจัดเก็บข้อมูลอย่างเดียว (ไม่มีบล้อก get/set)
+     เราควรป้องกันการเข้าถึงจากภายนอกคลาสด้วย private
+
+     class Triangle {
+    private var base: Int
+    private var height: Int
+
+    constructor(base: Int, height: Int) {
+        this.base = if (base > 0) base else 0
+        this.height = if (height > 0) height else 0
+    }
+
+    fun getArea(): Double = 0.5 * base * height
+}
+
+val t = Triangle(10, 15)
+   // t.base = 12 // Error private prop
+
+
+     ใช้ private กับ Primary Constructor แบบพร็อปเพอร์ตี้ มีผลในลักษณะเดียวกับ
+     พร็อปเพอร์ตี้แบบปกติ
+
+     class Circle(private var radius: Int) {
+     init {
          radius = if (radius > 0) radius else 0
-       }
      }
 
+     fun getArea() = 3.14 * radius * radius
 
+     fun getPerimeter() = 2 * 3.14 * radius
+ }
+
+ val c = Circle(10)
+    c.radius = 20 // Error private
+
+    กำหนด private ให้แก่เมธอด ไม่สามารถเข้าถึงได้จากภายนอกคลาส
+
+    class ThaiDate(private var day: Int,
+                private var month: Int,
+               private var year: Int) {
+
+     init {
+         if (!validDate(day, month, year)) {
+             day = 0; month = 0; year = 0
+         }
+     }
+
+     private val m = arrayOf("มกราคม")
+
+     fun getMonthName(): String? {
+         return if (month in 1..12) m[month - 1] else null
+     }
+
+     private fun validDate(d:Int, m:Int, y:Int): Boolean {
+         return try {
+             java.time.LocalDate.of(y, m, d)
+             true
+         } catch (ex: Exception) {
+             false
+         }
+     }
+
+ }
+
+   ใช้ private กับ Primary Constructor แบบพร็อปเพอร์ตี้
+    class Circle(private var radius: Int) {
+     init {
+         radius = if (radius > 0) radius else 0
+     }
+
+     fun getArea() = 3.14 * radius * radius
+
+     fun getPerimeter() = 2 * 3.14 * radius
+    }
+
+     val c = Circle(10)
+   // c.radius = 20 // Error private
 
 * */
+
 
 
 
