33260eeb68743f96e1c65078f94b30def87c23be
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-22 09:30:32 +0700

    Superclass มีเฉพาะ Primary Constructor

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,35 +2,26 @@ import kotlin.random.*
 
 /*
 
-    คอนสตรักเตอร์กับการสืบทอด
-    - ตามหลักการของ Kotlin เมื่อสืบทอดจากคลาสใด ต้องส่งข้อมูลให้แก่คอนสตรักเตอร์
-    ของคลาสนั้นเสมอ
+   Superclass มีเฉพาะ Primary Constructor
 
-    - กรณีที่ Superclass ไม่มีคอนสตรักเตอร์
-      ความจริงแล้ว ทุกๆ คลาสต้องมี Default Constructor เสมอ คอมไพเลอร์จะสร้างให้เอง
-      โดยอัตโนมัติ
+   - กำหนดชื่อพร็อปเพอร์ตี้ให้แตกต่าง
 
-    - การสืบทอดคอนสตรักเตอร์ โดยที่ BaseClass ไม่มีคอนสตรักเตอร์แบบ พารามิเตอร์
-      ใส่เป็นวงเล็บ () ว่างๆ ต่อท้ายชื่อ Superclass หมายถึงการเรียก Default Constructor
-      ของ Superclass
+   open class Device(var os: String) {
+   }
 
-    - อีก 1 วิธีสร้างคอนสตรักเตอร์แบบ Secondary แล้วระบุคำสั่ง super() ต่อท้าย
+   class Tablet(var osName: String) : Device(osName) {
+     // ส่งพร็อปเพอร์ตี้ osName ให้แก่คอนสตรักเตอร์ของ Device
+     // กำหนดชื่อที่ subclass ให้ต่างกันกับ base class ที่สืบทอด
+   }
 
-    open class Person {
-      // มีเฉพาะ Default Constructor
-    }
+   class Smartphone(osName: String, brandName: String)
+    : Device(osName) {
 
-    class Programmer: Person() { // เรียก Default constructor ของ Person
-    }
+   }
 
-    class Customer: Person { // ไม่ต้องมีวงเล็บ
-     constructor(name: String) : super() {} // เรียก Default Constructor
-    }
-
-    class Guest: Person {
-     // ในคอนสตรักเตอร์ ไม่มีคำสั่งใดๆ ไม่ต้องเขียนบล็อก {} ก็ได้
-     constructor() : super()
-    }
+   class Phablet : Device { // seconday constructor
+     constructor(osName: String) : super(osName) {}
+   }
 
 * */
 
