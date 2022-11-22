24b722d9c0d0b138c8409955d9eddea328a8f5ca
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-22 09:20:57 +0700

    คอนสตรักเตอร์กับการสืบทอด
    - กรณีที่ Superclass ไม่มีคอนสตรักเตอร์

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,90 +2,41 @@ import kotlin.random.*
 
 /*
 
-    Inheritance & Interface
+    คอนสตรักเตอร์กับการสืบทอด
+    - ตามหลักการของ Kotlin เมื่อสืบทอดจากคลาสใด ต้องส่งข้อมูลให้แก่คอนสตรักเตอร์
+    ของคลาสนั้นเสมอ
 
-    Open Class และ การสืบทอด
-     - คลาสที่จะสืบทอดได้ต้องระบุโมดิฟายเออร์ open กำกับเอาไว้
-     - คลาสที่จะสืบทอดเรียกว่า Superclass หรือ Base Class
-     - คลาสที่จะสืบทอด Base Class เรียกว่า Subclass
-     - ใช้เครื่องหมาย : ในการสืบทอด
+    - กรณีที่ Superclass ไม่มีคอนสตรักเตอร์
+      ความจริงแล้ว ทุกๆ คลาสต้องมี Default Constructor เสมอ คอมไพเลอร์จะสร้างให้เอง
+      โดยอัตโนมัติ
 
-    class Subclass : Superclass () {
-      ...
-    }
+    - การสืบทอดคอนสตรักเตอร์ โดยที่ BaseClass ไม่มีคอนสตรักเตอร์แบบ พารามิเตอร์
+      ใส่เป็นวงเล็บ () ว่างๆ ต่อท้ายชื่อ Superclass หมายถึงการเรียก Default Constructor
+      ของ Superclass
 
-    class NewClass : OldClass {
-    }
-
-    - Kotlin ไม่สนับสนุนการสืบทอดจากหลายคลาส (Multiple Inheritance)
-
-    open class A {
-    }
-
-    open class B : A() {
-    }
-
-    class C : B() {
-    }
-
-    class D : C() {} // Error ไม่ระบุ โมดิฟายเออร์ open จึงสืบทอดไม่ได้
-
-    ตัวอย่างของการไม่ใช้การสืบทอดของคลาส
+    - อีก 1 วิธีสร้างคอนสตรักเตอร์แบบ Secondary แล้วระบุคำสั่ง super() ต่อท้าย
 
     open class Person {
+      // มีเฉพาะ Default Constructor
+    }
 
-    var name: String = ""
-    var yearBorn: Int = 0
+    class Programmer: Person() { // เรียก Default constructor ของ Person
+    }
 
-    fun getAge() = java.time.LocalDate.now().year - yearBorn
+    class Customer: Person { // ไม่ต้องมีวงเล็บ
+     constructor(name: String) : super() {} // เรียก Default Constructor
+    }
 
-}
-
-// คลาส Person แทนลักษณะทั่วไปของบุคคล
-
-class Programmer {
-    var name: String = ""
-    var yearBorn: Int = 0
-    var salary: Int = 0
-    var expertLanguage: String = ""
-    fun getAge() = java.time.LocalDate.now().year - yearBorn
-}
-
-// คลาส Programmer แทนลักษณะของบุคคลที่เป็นโปรแกรมเมอร์
+    class Guest: Person {
+     // ในคอนสตรักเตอร์ ไม่มีคำสั่งใดๆ ไม่ต้องเขียนบล็อก {} ก็ได้
+     constructor() : super()
+    }
 
 * */
-
-open class Person {
-
-    var name: String = ""
-    var yearBorn: Int = 0
-
-    fun getAge() = java.time.LocalDate.now().year - yearBorn
-
-}
-
-// คลาส Person แทนลักษณะทั่วไปของบุคคล
-
-// ใช้วิธีการสืบทอด เพื่อลดความซ้ำซ้อนกับคลาส Person นั้นก็เพราะโปรแกรมเมอร์
-// ก็เป็นบุคคลอีกประเภทหนึ่ง
-class Programmer: Person() {
-    var salary: Int = 0
-    var expertlanguage: String = ""
-}
-
-// คลาส Programmer แทนลักษณะของบุคคลที่เป็นโปรแกรมเมอร์
 
 
 fun main(args: Array<String>) {
 
-    val pro = Programmer()
-    pro.yearBorn = 1995
-    println(pro.getAge())
-    pro.salary = 50_000
 
-    val per = Person()
-    per.name = "James Gosling"
-//    per.salary = 40_000 // Error เพราะคลาส Person ไม่มีพร็อปเพอร์ตี้นี้
-//    per.expertLanguage = "Java" // Error เพราะคลาส Person ไม่มีพร็อปเพอร์ตี้นี้
 
 }
