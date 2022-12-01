284e4db7fe58111e19d729ace125358d952464b6
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-12-01 09:40:37 +0700

    การโอเวอร์ไรด์เมธอด

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,37 +2,27 @@ import kotlin.random.*
 
 /*
 
-  การโอเวอร์ไรด์สมาชิกของ Subclass
-  การโอเวอร์ไรด์พร็อปเพอร์ตี้ เหมือนกับการไปปิดบัง (hide) พร็อปเพอร์ตี้ตัวนั้น
-  ใน Superclass ให้มองเห็นเป็นตัวใหม่ที่สร้างขึ้นใน Subclass แทน
-
-  - ที่ Superclass ระบุโมดิฟายเออร์ open นำหน้าพร็อปที่สามารถโอเวอร์ไรด์ได้
-  - Subclass ระบุโมดิฟายเออร์ override พร้อมกำหนดชื่อพร็อปเพอร์ตี้และชนิดข้อมูล
-  ให้ตรงกับของ Superclass (ใช้ Type Inference ได้)
-
-  - คอนสตรักเตอร์แบบพร็อปเพอร์ตี้ ใช้หลักการเดียวกัน
-
+   การโอเวอร์ไรด์เมธอด
+   - ที่ Superclass เมธอดนั้น ต้องระบุโมดิฟายเออร์ open
+   - Subclass ต้องกำหนดชื่อ เมธอด และ พารามิเตอร์ให้ตรงกับใน Superclass
+   (ถ้าพารามิเตอร์ไม่ตรงกัน ชื่อ เมธอดตรงกัน เป็นการโอเวอร์โหลด ไม่ใช้โอเวอร์ไรด์)
 
 * */
 
- open class Java {
-     open val version: Double = 15.0
-     open val compiler = "Java Compiler"
-     val fileExtension = ".java"
-     val owner = "Oracle"
+ open class Shape {
+     open fun area() = 0.0
+     fun name() = "Shape"
  }
 
- class Kotlin: Java() {
-     override val version: Double = 2.0
-     override var compiler = "Kotlin Compiler"
-     // override val fileExtension = ".kt" // Error
-     // val owner = "Jet" // Error
+ class Circle(var radius: Int) : Shape() {
+     override fun area() = 3.141 * radius * radius
+
+     fun area(pi: Double = 3.14) = pi * radius * radius
+     // เมธอดตรงกัน แต่ พารามิเตอร์ไม่ตรงกัน เป็นการโอเวอร์โหลด
+     // สามารถทำได้
+     // override fun name() = "GG" // Error
  }
 
- open class Device(open var os: String)
- class Smartphone(override var os: String) : Device(os)
- // ที่ Superclass ระบุโมดิฟายเออร์ open ดังนั้น ที่ Subclass จึงสามารถ
- // โอเวอร์ไรด์และกำหนดชื่อพร็อปเพอร์ตี้ให้เหมือนกันได้
 
 
 fun main(args: Array<String>) {
