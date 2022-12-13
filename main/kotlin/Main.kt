f8218700f22ce27ce949b2ce01194ce25250032d
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-12-13 21:26:15 +0700

    วิธีการ Export โปรเจ็กต์เป็นไฟล์ Zip

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -2,74 +2,23 @@ import kotlin.random.*
 
 /*
 
-   Object Expression
-   - หากต้องการนำอินเทอร์เฟซไปใช้งาน ต้องสร้างคลาสขึ้นมาสืบทอด ซึ้งอาจเกิดความ
-   ยุ่งยาก
-   - ลักษณะของ Object Expression คือใช้คีย์เวิร์ด object แทนคลาสที่จะสืบทอดจาก
-   อินเทอร์เฟซโดยไม่ต้องระบุชื่อในรูปแบบดังนี้
+    การตรวจสอบชนิดข้อมูลด้วย is และ !is
+    - is ตรวจสอบตัวแปรหรืออินสแตนซ์ เป็นชนิดที่ระบุหรือไม่ หากใช้จะคืนค่า true
+    - !is จะใช้ในทางตรงกันข้ามคือ หากตัวแปรหรืออินสแตนซ์ ไม่ใช้ชนิดที่ระบุ จะคืนค่า true
 
-   object: interface_name {
-     // โอเวอร์ไรด์สมาชิกของอินเทอร์เฟซที่เป็น Abstract ตามปกติ
-   }
+    วิธีการ Export โปรเจ็กต์เป็นไฟล์ Zip
+    - เป็นวิธีการจัดเก็บโปรเจ็กต์ Android Apps ของเราให้มีขนาดเล็กลง
+    ส่งผลให้โปรเจคที่ Export ออกมาอยู่ในสถานะที่ใช้งานไม่ได้ชั่วคราว
+    - เวลาเปิดโปรเจ็กต์ (ที่อยู่ในสถานะใช้งานไม่ได้ชั่วคราว) จะต้องมีการ
+    ดาวน์โหลดและสร้างไฟล์ชั่วคราวต่างๆ ขึ้นมาใหม่ เพื่อให้โปรเจ็กต์กลับมาอยู่ในสถานะใช้งาน
+    ได้ตามปกติ
 
-   - กรณีที่เราต้องการนำไปใช้งานโดยตรง อาจสร้างตัวแปรเพื่อใช้อ้างถึงออบเจ็กต์
-   ดังกล่าว เช่น
-   interface Math {
-    fun add(a: Int, b: Int) : Int
-    fun subtract(a: Int, b: Int) : Int
-    }
-    var mt = object: Math {
-        override fun add(a: Int, b: Int): Int = a + b
-        override fun subtract(a: Int, b: Int) = a - b
-    }
-
-    - ลักษณะที่ฟังก์ชัน (หรือ เมธอดของคลาส) รับพารามิเตอร์ที่เป็นแบบอินเทอร์เฟซ
-      - สร้างออบเจ็กต์ ของอินเทอร์เฟซพร้อมโอเวอร์ไรด์ แล้วนำไปกำหนดให้แก่พารา
-      มิเตอร์ตัวนั้น เช่น
-      var mt = object: Math {
-        override fun add(a: Int, b: Int): Int = a + b
-        override fun subtract(a: Int, b: Int) = a - b
-    }
-
-    calculate(20, 5, mt)
-
-    - หรือนำออบเจ็กต์ของอินเทอร์เฟซมาใส่เป็นอาร์กิวเมนต์โดยตรง เพื่อลดทอนขั้นตอนที่
-    ต้องสร้างเป็นตัวแปรขึ้นมาก่อน
 
 * */
 
-interface Math {
-    fun add(a: Int, b: Int) : Int
-    fun subtract(a: Int, b: Int) : Int
-}
-
-interface MouseClick {
-    fun onclick()
-}
-
-fun calculate(x: Int, y: Int, math: Math) { // Math เป็นอินเทอร์เฟซ
-    println("$x + $y = ${math.add(x, y)}")
-    println("$x - $y = ${math.subtract(x, y)}")
-}
-
-class Button {
-    fun onMouseClick(m: MouseClick) {
-        m.onclick()
-    }
-}
 
 fun main(args: Array<String>) {
 
-    calculate(20, 5, object: Math {
-        override fun add(a: Int, b: Int): Int = a + b
-        override fun subtract(a: Int, b: Int): Int = a - b
-    })
 
-    val bt = Button()
-    bt.onMouseClick(object: MouseClick {
-        override fun onclick() {
-            println("Mouse Click")
-        }
-    })
 
 }
