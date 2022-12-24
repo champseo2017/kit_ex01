fed5c9ddb54d664da900e2fa53882bd682f6d75d
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-12-24 10:39:48 +0700

    การตรวจสอบชนิดข้อมูลด้วย is และ !is

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -6,20 +6,89 @@ import kotlin.random.*
     - is ตรวจสอบตัวแปรหรืออินสแตนซ์ เป็นชนิดที่ระบุหรือไม่ หากใช้จะคืนค่า true
     - !is จะใช้ในทางตรงกันข้ามคือ หากตัวแปรหรืออินสแตนซ์ ไม่ใช้ชนิดที่ระบุ จะคืนค่า true
 
-    - ถ้า Element มีวงลกมแบบโปร่งใสเกาะอยู่ทั้ง 4 ด้าน หมายความว่า
-    ปุ่มนี้ยังไม่มีการกำหนดให้ยึดติดกับ ConstraintLayout ทุกด้าน
+    val a: Int = 10
+  var bool = a is Int // true
+  bool = a !is Double // Error เปรียบเทียบกันไม่ได้
+  bool = a !is Long // Error เปรียบเทียบกันไม่ได้
+  val b = 1.23
+  bool = b is Double // true
+  bool = b is Float // Error
 
-    ConstraintLayout มีเพียง 2 อย่างเท่านั้นคือ
-     - กำหนดตำแหน่งตามแนวนอน (แกน x)
-     - กำหนดตำแหน่งตามแนวตั้ง (แกน y)
+  var x: Any = 10 // Any เปรียบเทียบกับชนิดใดก็ได้
+  println(x is Int) // true
+  println(x is Double) // false
+  println(x !is String) // true
+  x = "Hello"
+  println(x is Int) // false
+  println(x is String) // true
+  println(x is Any) // true
 
-     - ถ้าปุ่มกด Button มีวงกลมทึบด้านซ้าย และ ด้านบนอย่างละ 1 ตำแหน่ง
-      หมายความว่า มีการกำหนดให้ยึดติดกับ ConstraintLayout ครบตามกติกาแล้ว
+  fun test(a: Any) {
+      if (a is Int) {
+          ...
+      } else if (a is Double) {
+
+      }
+       หรือใช้ when เช่น
+       when (a) {
+        is Int -> ...
+       is Double -> ...
+       }
+  }
+
+  ลักษณะการเปรียบเทียบในแบบอินสแตนซ์ของคลาส
+    class One
+class Two
+
+val a = One()
+    val b = Two()
+    val x = a is One // true
+    // if (b is Two) {...} // true
+
+    open class Tea
+class IceTea: Tea()
+class Coffee
+
+val t = Tea()
+    println(t is Tea) // true
+    println(t is IceTea) // false
+
+    val it = IceTea()
+    println(it is Tea) // true
+    println(it is IceTea) // true
+
+    val cff = Coffee()
+    println(cff is Tea) // Error
 
 * */
 
 
+open class Tea
+class IceTea: Tea()
+
+fun drink(a: Any) {
+    println(when (a) {
+        is IceTea -> "IceTea"
+        is Tea -> "Tea"
+        else -> ""
+    })
+}
+
+fun drink2(d: Tea) {
+    println(when (d) {
+        is Tea -> "Tea"
+        is IceTea -> "IceTea"
+        else -> ""
+    })
+}
+
+
 fun main(args: Array<String>) {
+
+    val t = Tea()
+    val it = IceTea()
+    drink(t) // Tea
+    drink(it) // IceTea
 
 
 
