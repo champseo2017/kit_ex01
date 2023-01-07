f243b7953ff108085d987190d1980ca0ca352736
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-01-07 15:12:17 +0700

    คลาสแบบ Generic 1

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,30 +1,52 @@
-import kotlin.random.*
+import java.util.*
+import java.*;
+import kotlin.random.*;
 
 /*
+*  คลาสแบบ Generic
+*  Generic Class เป็นการสร้างคลาสที่สามารถรองรับและจัดการข้อมูลได้หลายชนิด
+*
+*  - ใน Kotlin จะมี default type เป็นชนิด any อาจมีปัญหาเรื่อง Nullable ในขณะรัน
+* หากเรานำอินสแตนซ์ของ Generic Type ไปใช้งานโดยตรง ลองแปลงจาก Generic Type
+* ไปเป็นชนิด Any
+*
+* - เมื่อนำ Generic class ไปใช้งานในขั้นตอนการสร้างอินสแตนว์ต้องระบุชนิดข้อมูลที่แน่นอนให้กับคลาส
+*  ซึ้งสามารถกำหนดได้หลายแบบ หรือ ถ้าไม่ระบุชนิดข้อมูล คอมไพเลอร์จะพิจารณาจากค่าที่กำหนดให้แก่คอน
+* สตรักเตอร์
+*
+* val data1: Data<Int> = Data<Int>(100)
+  val data2: Data<Double> = Data(12.34)
+  val data3 = Data<String>("Hello")
+  val data4 = Data(true) // มีชนิดข้อมูลเป็น Boolean
+  print(data1.type())
 
-    การใช้ as? (Safe Cast Operator)
-    - เป็นการแปลงชนิดข้อมูลที่ปลอดภัยกว่า เปลียนจาก as ไปใช้ as? แทน
-    ซึ้ง as? คือ หากแปลง Type ไม่ได้ จะคืนค่า null ดังนั้นต้องนำวิธีการแบบ
-    Nullable มาใช้ร่วมด้วย
+  - Type ที่จะกำหนดให้แก่ Generic class ไม่ใช้ชนิดข้อมูลพื้นฐาน เราต้องกำหนดค่าเป็นอินสแตนซ์
+  ของคลาสดังกล่าวให้กับคอนสตรักเตอร์
+  * val test = Test()
+    val data1 = Data<Test>(test)
+    val data2 = Data<Test>(Test())
+    val data3 = Data(Test())
 
+   - อินสแตนซ์เดิมซึ่งได้กำหนดชนิดข้อมูล Generic Type เป็นอย่างใดอย่างหนึ่งไปแล้ว
+   * ไม่สามารถนำมาสร้างเพื่อกำหนด Type ใหม่ได้
+   * var data = Data<Int>(value = 123)
+   data = Data<String>(value = "Kotlin") // Error
+   data = Data(value = true) // Error
+*
+*
 * */
-val a: Int = 10
-val b: Double? = a as? Double // null
-val c = "123"
-val d: Int? = (c as? Int) ?: 0
 
-fun test(x: Any) {
-    var y: Double? = x as? Double
-    val z: Int? = x as? Int
+class Data<T>(value: T) {
+    private var v = value as Any
+    fun type() = v::class.simpleName
+    fun value(): T = v as T
+    fun setValue(value: T) {
+        v = value as Any
+    }
 }
 
-open class Tea
-class IceTea : Tea()
+class Test
 
 fun main(args: Array<String>) {
-
- var t = Tea()
- var it: IceTea? = t as? IceTea // null
-
 
 }
